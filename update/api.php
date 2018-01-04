189dcc7c3ac946f9fbacf309aad4b120fc403eb8
Thomas Zilio <Dasc3er@users.noreply.github.com>
2018-01-04 16:59:05 +0100

    Fix dell'aggiornamento (#97)

--- update/api.php
+++ update/api.php
@@ -92,7 +92,7 @@ $tables = [
 
 foreach ($tables as $table) {
     if ($database->fetchNum('SHOW TABLES WHERE `Tables_in_'.$database->getDatabaseName().'` = '.prepare($table))) {
-        $query = 'SHOW COLUMNS FROM '.$table.' FROM '.$database->getDatabaseName()." WHERE Field='|field|'";
+        $query = 'SHOW COLUMNS FROM `'.$table.'` IN `'.$database->getDatabaseName()."` WHERE Field='|field|'";
 
         $created_at = $database->fetchArray(str_replace('|field|', 'created_at', $query));
         if (empty($created_at)) {

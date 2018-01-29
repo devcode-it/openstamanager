233b7d96e78de1150c616d7c12d5b4231777dd60
Fabio Lovato <fabiol@openstamanager.com>
2018-01-29 15:07:51 +0100

    Corretta vista duplicata delle sedi

---
+++ update/2.3.1.sql
@@ -0,0 +1,2 @@
+-- Rimosso il referente dalla lista sedi, per non mostrare sedi duplicate nella lista sedi
+UPDATE `zz_plugins` SET `options` = '	{ "main_query": [	{	"type": "table", "fields": "Nome, Indirizzo, Città, CAP, Provincia",	"query": "SELECT an_sedi.id, an_sedi.nomesede AS Nome, an_sedi.indirizzo AS Indirizzo, an_sedi.citta AS Città, an_sedi.cap AS CAP, an_sedi.provincia AS Provincia FROM an_sedi WHERE 1=1 AND an_sedi.idanagrafica=|idanagrafica| HAVING 2=2 ORDER BY an_sedi.id DESC"}	]}', `created_at` = NULL WHERE `zz_plugins`.`name` = 'Sedi';

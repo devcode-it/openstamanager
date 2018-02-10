9d0bfa0f09f4d4b527be106b56904ef1b2986b02
Thomas Zilio <Dasc3er@users.noreply.github.com>
2018-02-10 18:43:17 +0100

    Bugfix #148

--- update/2_3_1.sql
+++ update/2_3_1.sql
@@ -7,3 +7,10 @@ ALTER TABLE `zz_files` ADD `id_plugin` int(11) AFTER `id_module`;
 -- Aggiunto valore NULL a idarticolo in co_righe2_contratti
 -- ALTER TABLE `co_righe2_contratti` CHANGE `idarticolo` `idarticolo` int(11);
 -- UPDATE `co_righe2_contratti` SET `idarticolo` = NULL WHERE `idarticolo` = 0;
+
+-- Aggiornamento ORDER BY nei moduli Fatture, DDT, Ordini, Preventivi, Contratti e Interventi
+UPDATE `zz_views` SET `order_by` = 'CAST(numero_esterno AS UNSIGNED)' WHERE `name` = 'Numero' AND `id_module` IN (SELECT `id` FROM `zz_modules` WHERE `name` IN ('Fatture di vendita', 'Fatture di acquisto', 'Ordini cliente', 'Ordini fornitore', 'Ddt di vendita', 'Ddt di acquisto'));
+
+UPDATE `zz_views` SET `order_by` = 'CAST(in_interventi.codice AS UNSIGNED)' WHERE `name` = 'Numero' AND `id_module` = (SELECT `id` FROM `zz_modules` WHERE `name` = 'Interventi');
+
+UPDATE `zz_views` SET `order_by` = 'CAST(numero AS UNSIGNED)' WHERE `name` = 'Numero' AND `id_module` IN (SELECT `id` FROM `zz_modules` WHERE `name` IN ('Ordini Preventivi', 'Contratti'));

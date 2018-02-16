79c6137bb1469a545452a30ccdc20055a00758f9
Luca <lucasalva87@users.noreply.github.com>
2018-02-16 13:16:17 +0100

    Aggiunta la possibilità di cambiare la descrizione per alcuni stati intervento

--- modules/stati_intervento/actions.php
+++ modules/stati_intervento/actions.php
@@ -14,7 +14,7 @@ switch (post('op')) {
         $query = 'UPDATE in_statiintervento SET descrizione='.prepare($descrizione).' WHERE idstatointervento='.prepare($id_record).' AND `default`=0';
         $dbo->query($query);
 
-        $_SESSION['infos'][] = tr('Informazioni salvate correttamente!');
+        $_SESSION['infos'][] = tr('Informazioni salvate correttamente.');
 
         break;
 
@@ -22,20 +22,32 @@ switch (post('op')) {
         $idstatointervento = post('idstatointervento');
         $descrizione = post('descrizione');
         $colore = post('colore');
+		
+		
+	
+		if (count($dbo->fetchArray('SELECT idstatointervento FROM in_statiintervento WHERE idstatointervento='.prepare($idstatointervento).''))>0){
+			
+		   $_SESSION['errors'][] = tr('Stato di intervento già esistente.');
+			   
+		}else{
+							
+							
+			$query = 'INSERT INTO in_statiintervento(idstatointervento, descrizione, colore) VALUES ('.prepare($idstatointervento).', '.prepare($descrizione).', '.prepare($colore).')';
+			$dbo->query($query);
+			$id_record = $idstatointervento;
 
-        $query = 'INSERT INTO in_statiintervento(idstatointervento, descrizione, colore) VALUES ('.prepare($idstatointervento).', '.prepare($descrizione).', '.prepare($colore).')';
-        $dbo->query($query);
-        $id_record = $idstatointervento;
-
-        $_SESSION['infos'][] = tr('Nuovo stato di intervento aggiunto!');
-
+			$_SESSION['infos'][] = tr('Nuovo stato di intervento aggiunto.');
+		
+		}
+		
         break;
 
     case 'delete':
-        $query = 'DELETE FROM in_statiintervento WHERE idstatointervento='.prepare($id_record).' AND `default`=0';
-        $dbo->query($query);
+        $query = 'UPDATE in_statiintervento SET deleted = 1 WHERE idstatointervento='.prepare($id_record).' AND `default`=0';
+        
+		$dbo->query($query);
 
-        $_SESSION['infos'][] = tr('Stato di intervento eliminato!');
+        $_SESSION['infos'][] = tr('Stato di intervento eliminato.');
 
         break;
 }

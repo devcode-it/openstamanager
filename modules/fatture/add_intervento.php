8b88b4b58d233c22aa9ae4e7f5dec0f0fb5423a8
Luca <lucasalva87@users.noreply.github.com>
2018-02-16 11:56:08 +0100

    Fix ordinamento voci di menù + modificate query per filtro widget in base al gruppo

--- modules/fatture/add_intervento.php
+++ modules/fatture/add_intervento.php
@@ -42,11 +42,11 @@ echo '
     <div class="row">
 	
         <div class="col-md-6">
-            {[ "type": "select", "label": "'.tr('Intervento').'", "name": "idintervento", "required": 1, "values": '.json_encode($rs).', "extra": "onchange=\"$data = $(this).selectData(); $(\'#descrizione\').val($data.descrizione); if($(\'#descrizione_intervento\').is(\':checked\')){  $(\'#descrizione\').val($data.descrizione + $data.descrizione_intervento); }; $(\'#prezzo\').val($data.prezzo);\"" ]}
+            {[ "type": "select", "label": "'.tr('Intervento').'", "name": "idintervento", "required": 1, "values": '.json_encode($rs).', "extra": "onchange=\"$data = $(this).selectData(); $(\'#descrizione\').val($data.descrizione); if($(\'#copia_descrizione\').is(\':checked\')){  $(\'#descrizione\').val($data.descrizione + $data.descrizione_intervento); }; $(\'#prezzo\').val($data.prezzo);\"" ]}
         </div>
 		
 		<div class="col-md-6">
-            {[ "type": "checkbox", "label": "'.tr('Descrizione').'", "name": "descrizione_intervento", "required": 0, "values": "", "extra": "", "help": "", "placeholder": "'.tr('Copia la descrizione dell\'intervento').'." ]}
+            {[ "type": "checkbox", "label": "'.tr('Copia descrizione').'", "name": "copia_descrizione", "required": 0, "values": "", "extra": "", "help": "", "placeholder": "'.tr('In fase di selezione copia la descrizione dell\'intervento').'." ]}
         </div>
 
     </div>';

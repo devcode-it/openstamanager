231b1b5f9d13fb37595718591e24a9930efb4015
Fabio Lovato <fabiol@openstamanager.com>
2018-02-16 11:36:38 +0100

    Migliorati i permessi per la stampa fattura

--- templates/fatture/init.php
+++ templates/fatture/init.php
@@ -58,7 +58,10 @@ $custom = [
     'c_destinazione' => $destinazione,
 ];
 
-// Controllo sui permessi
-if ($id_cliente != Auth::user()['idanagrafica'] && !Auth::admin()) {
+// Accesso solo a:
+// - cliente se è impostato l'idanagrafica di un Cliente
+// - utente qualsiasi con permessi almeno in lettura sul modulo
+// - admin
+if ( ( !empty(Auth::user()['idanagrafica']) && $id_cliente != Auth::user()['idanagrafica'] && !Auth::admin()) || Modules::getPermission($module_name)=='-') {
     die(tr('Non hai i permessi per questa stampa!'));
 }

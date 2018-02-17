c174c57d472df8360d56d702ce8f85b20e421562
Fabio Lovato <fabiol@devcode.it>
2018-02-17 16:08:10 +0100

    Separati indirizzo e città nell'intestazione delle stampe

--- templates/base/header.php
+++ templates/base/header.php
@@ -16,7 +16,8 @@ return '
     </div>
     <div class="col-xs-6 text-right">
         <p><b>'.$f_ragionesociale.'</b></p>
-        <p>'.$f_indirizzo.' '.$f_citta_full.'</p>
+        <p>'.$f_indirizzo.'</p>
+        <p>'.$f_citta_full.'</p>
         <p>'.(!empty($f_piva) ? tr('P.Iva').': '.$f_piva : '').'</p>
         <p>'.(!empty($f_codicefiscale) ? tr('C.F.').': '.$f_codicefiscale : '').'</p>
         <p>'.(!empty($f_capsoc) ? tr('Cap.Soc.').': '.$f_capsoc : '').'</p>

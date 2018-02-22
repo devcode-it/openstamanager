febc7b65498a406a71fc38fd814791e61a0495d7
loviuz <loviuz@localhost.localdomain>
2018-02-22 23:18:53 +0100

    Aggiunte variabili di default nelle attività

--- modules/interventi/variables.php
+++ modules/interventi/variables.php
@@ -1,7 +1,11 @@
 <?php
 
-$rs = $dbo->fetchArray('SELECT * FROM in_interventi WHERE id='.prepare($id_record))[0];
+$rs = $dbo->fetchArray('SELECT *, (SELECT MAX(orario_fine) FROM in_interventi_tecnici WHERE idintervento=in_interventi.id) AS data_fine FROM in_interventi WHERE id='.prepare($id_record))[0];
 
 return [
-    'id_anagrafica' => $rs['idanagrafica'],
+    'codice' => $rs['codice'],
+    'richiesta' => $rs['richiesta'],
+    'descrizione' => $rs['descrizione'],
+    'data richiesta' => date( 'd/m/Y', strtotime($rs['data_richiesta']) ),
+    'data fine intervento' => ( empty($rs['data_fine']) ? date('d/m/Y', strtotime($rs['data_richiesta'])) : date('d/m/Y', strtotime($rs['data_fine'])) ),
 ];

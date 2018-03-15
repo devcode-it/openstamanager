e6e0675e20018a38f904ebd3480f0e6da547cb0a
Thomas Zilio <Dasc3er@users.noreply.github.com>
2018-03-15 17:05:38 +0100

    Fix del commit precedente
    Utilizzo della funzione prepareToField per effettuare automaticamente la sostituzione degli apici problematici.

--- modules/viste/edit.php
+++ modules/viste/edit.php
@@ -146,7 +146,7 @@ if (!empty($options) && $options != 'custom') {
 								</div>
 								<div class="row">
 									<div class="col-xs-12 col-md-12">
-										{[ "type": "textarea", "label": "'.tr('Query prevista').'", "name": "query['.$key.']", "value": "'.str_replace( '"', '\"', $field['query'] ).'"';
+										{[ "type": "textarea", "label": "'.tr('Query prevista').'", "name": "query['.$key.']", "value": "'.prepareToField($field['query']).'"';
         if (!$editable) {
             echo ', "readonly": "1"';
         }
@@ -384,7 +384,7 @@ if (!empty($options) && $options != 'custom') {
                                     <input type="hidden" value="'.$additional['id'].'" name="id['.$num.']">
 
                                     <div class="col-xs-12 col-md-6">
-                                        {[ "type": "textarea", "label": "'.tr('Query').'", "name": "query['.$num.']", "value": "'.$additional['clause'].'"';
+                                        {[ "type": "textarea", "label": "'.tr('Query').'", "name": "query['.$num.']", "value": "'.prepareToField($additional['clause']).'"';
         if (!$editable) {
             echo ', "readonly": "1"';
         }

c35e1d97461da152eada82e05257f61f46a3f8ba
Thomas Zilio <Dasc3er@users.noreply.github.com>
2018-03-24 14:08:10 +0100

    Spostamento della 2.3.2 nella 2.4

--- update/2_3_2.sql
+++
@@ -1,9 +0,0 @@
--- Lo stato 'FAT' è da considerarsi completato 
-UPDATE `in_statiintervento` SET `completato` = '1' WHERE `in_statiintervento`.`idstatointervento` = 'FAT';
-
--- Nuovi campi per iva su righe 'Materiale utilizzato' in interventi
-ALTER TABLE `mg_articoli_interventi` CHANGE `idiva_vendita` `idiva` INT(11) NOT NULL;
-ALTER TABLE `mg_articoli_interventi` ADD `desc_iva` VARCHAR(255) NOT NULL AFTER `idiva`, ADD `iva` DECIMAL(12,4) NOT NULL AFTER `desc_iva`;
-
--- Nuovi campi per iva su righe 'Altre spese' in interventi
-ALTER TABLE `in_righe_interventi` ADD `idiva` INT(11) NOT NULL AFTER `prezzo_acquisto`, ADD `desc_iva` VARCHAR(255) NOT NULL AFTER `idiva`, ADD `iva` DECIMAL(12,4) NOT NULL AFTER `desc_iva`; 

f5e60103899a7964ca6478aaa561677e60b9792b
Bacca97 <matteo.bacca@gmail.com>
2018-05-23 11:32:46 +0200

    Bugfix

--- modules/interventi/ajax/select.php
+++ modules/interventi/ajax/select.php
@@ -4,7 +4,7 @@ include_once __DIR__.'/../../../core.php';
 
 switch ($resource) {
     case 'tipiintervento':
-        $query = 'SELECT idtipointervento AS id, IF(tempo_standard IS NULL, descrizione, CONCAT(descrizione, \' (\', REPLACE(FORMAT(tempo_standard, 2), \'.\', \',\'), \' ore)\')) AS descrizione, tempo_standard FROM in_tipiintervento |where| ORDER BY idtipointervento';
+        $query = 'SELECT idtipointervento AS id, CASE WHEN ISNULL(tempo_standard) OR tempo_standard <= 0 THEN descrizione WHEN tempo_standard > 0 THEN  CONCAT(descrizione, \' (\', REPLACE(FORMAT(tempo_standard, 2), \'.\', \',\'), \' ore)\') END AS descrizione, tempo_standard FROM in_tipiintervento |where| ORDER BY idtipointervento';
 
         foreach ($elements as $element) {
             $filter[] = 'idtipointervento='.prepare($element);

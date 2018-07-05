7bd7ae6ba111753e78046d32461ed03715549a3f
Thomas Zilio <Dasc3er@users.noreply.github.com>
2018-07-05 10:07:39 +0200

    Aggiornamento nazioni

--- update/2_4_2.sql
+++ update/2_4_2.sql
@@ -1,10 +1,10 @@
--- Gestione documentale 
+-- Gestione documentale
  CREATE TABLE IF NOT EXISTS `zz_documenti_categorie` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `descrizione` varchar(255) NOT NULL,
   `deleted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS `zz_documenti` (
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `zz_documenti` (
   `nome` varchar(255) NOT NULL,
   `data` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB;
 
 
 INSERT INTO `zz_documenti_categorie` (`id`, `descrizione`, `deleted`) VALUES
@@ -25,3 +25,6 @@ INSERT INTO `zz_modules` (`id`, `name`, `title`, `directory`, `options`, `option
 
 -- Innesto modulo categorie documenti
 INSERT INTO `zz_modules` (`id`, `name`, `title`, `directory`, `options`, `options2`, `icon`, `version`, `compatibility`, `order`, `parent`, `default`, `enabled`) VALUES (NULL, 'Categorie documenti', 'Categorie documenti', 'categorie_documenti', '{	"main_query": [	{	"type": "table", "fields": "Descrizione", "query": "SELECT zz_documenti_categorie.`descrizione`as Descrizione, zz_documenti_categorie.`id`as id FROM zz_documenti_categorie WHERE deleted = 0 HAVING 1=1"}	]}', '', 'fa fa-file-text-o', '2.4', '2.4', '1', (SELECT `id` FROM `zz_modules` m WHERE `name` = 'Gestione documentale'), '1', '1');
+
+-- Fatturazione elettronica
+ALTER TABLE `an_nazioni` ADD `name` VARCHAR(255);

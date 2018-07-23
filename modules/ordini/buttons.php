5d124c73e479b7da091f34d7c6e1f91dc960c23b
Luca <lucasalva87@users.noreply.github.com>
2018-07-23 14:35:59 +0200

    Bugfix: possibilità di fatturare il rimanente degli ordini parzialmente fatturati.

--- modules/ordini/buttons.php
+++ modules/ordini/buttons.php
@@ -2,25 +2,32 @@
 
 include_once __DIR__.'/../../core.php';
 
-if (!in_array($records[0]['stato'], ['Evaso', 'Fatturato', 'Parzialmente fatturato'])) {
+if (!in_array($records[0]['stato'], ['Evaso', 'Fatturato'])) {
     echo '
 	<div class="dropdown">
 	<button class="btn btn-info dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
 		<i class="fa fa-magic"></i>&nbsp;'.tr('Crea').'...
 		<span class="caret"></span>
 	</button>
-	<ul class="dropdown-menu dropdown-menu-right">
-
+	<ul class="dropdown-menu dropdown-menu-right">';
+	
+	if (in_array($records[0]['stato'], ['Parzialmente evaso'])){	
+	echo '
 			<li>
 				<a data-href="'.$rootdir.'/modules/fatture/crea_documento.php?id_module='.$id_module.'&id_record='.$id_record.'&documento=ddt" data-toggle="modal" data-title="'.tr('Crea ddt').'" data-target="#bs-popup"><i class="fa fa-file-o"></i>&nbsp;'.tr('ddt').'
 				</a>
-			</li>
-
+			</li>';
+	}
+	
+	if (in_array($records[0]['stato'], ['Parzialmente fatturato'])){
+	echo '
 			<li>
 				<a data-href="'.$rootdir.'/modules/fatture/crea_documento.php?id_module='.$id_module.'&id_record='.$id_record.'&documento=fattura" data-toggle="modal" data-title="'.tr('Crea fattura').'" data-target="#bs-popup"><i class="fa fa-file"></i>&nbsp;'.tr('fattura').'
 				</a>
-			</li>
-
+			</li>';
+	}
+			
+	echo '
 		</ul>
 	</div>';
 }

1440e657ce30d1ae247a02794fa642a96816805e
Maicol <maicolbattistini@live.it>
2018-09-25 20:06:33 +0200

    Standardizzazione nomi dei nuovi campi e delle variabili (Fix)

--- update/2_4_2.sql
+++ update/2_4_2.sql
@@ -482,7 +482,7 @@ UPDATE `in_statiintervento` SET `id_email` = (SELECT `id` FROM `zz_emails` WHERE
 
 -- Ritenuta d'acconto predefinita per anagrafica
 ALTER TABLE `an_anagrafiche` ADD `id_ritenuta_acconto_vendite` INT(11) NULL DEFAULT NULL AFTER `idiva_acquisti`;
-ALTER TABLE `an_anagrafiche` ADD `id_ritenuta_acconto_acquisti` INT(11) NULL DEFAULT NULL AFTER `id_ritenutaacconto_vendite`;
+ALTER TABLE `an_anagrafiche` ADD `id_ritenuta_acconto_acquisti` INT(11) NULL DEFAULT NULL AFTER `id_ritenuta_acconto_vendite`;
 
 -- Correzione partite ive e codici fiscali
 UPDATE `an_anagrafiche` SET `piva` = REPLACE(`piva`, ' ', ''), `codice_fiscale` = REPLACE(`codice_fiscale`, ' ', '');

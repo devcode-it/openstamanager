41cfde77e4564e9f7b162b2acd7e7d0055e1fb8f
Luca <lucasalva87@users.noreply.github.com>
2018-10-24 16:49:02 +0200

    Fix ordinamento anno/mesi promemoria

--- modules/dashboard/edit.php
+++ modules/dashboard/edit.php
@@ -289,7 +289,7 @@ $qp = 'SELECT MONTH(data_richiesta) AS mese, YEAR(data_richiesta) AS anno FROM (
 
 UNION SELECT MONTH(data_scadenza) AS mese, YEAR(data_scadenza) AS anno FROM (co_ordiniservizio INNER JOIN co_contratti ON co_ordiniservizio.idcontratto=co_contratti.id) INNER JOIN an_anagrafiche ON co_contratti.idanagrafica=an_anagrafiche.idanagrafica WHERE idcontratto IN( SELECT id FROM co_contratti WHERE idstato IN(SELECT id FROM co_staticontratti WHERE pianificabile = 1) ) AND idintervento IS NULL
 
-UNION SELECT MONTH(data_richiesta) AS mese, YEAR(data_richiesta) AS anno FROM in_interventi INNER JOIN an_anagrafiche ON in_interventi.idanagrafica=an_anagrafiche.idanagrafica WHERE (SELECT COUNT(*) FROM in_interventi_tecnici WHERE in_interventi_tecnici.idintervento = in_interventi.id) = 0';
+UNION SELECT MONTH(data_richiesta) AS mese, YEAR(data_richiesta) AS anno FROM in_interventi INNER JOIN an_anagrafiche ON in_interventi.idanagrafica=an_anagrafiche.idanagrafica WHERE (SELECT COUNT(*) FROM in_interventi_tecnici WHERE in_interventi_tecnici.idintervento = in_interventi.id) = 0 ORDER BY anno,mese';
 $rsp = $dbo->fetchArray($qp);
 
 if (!empty($rsp)) {
@@ -336,8 +336,10 @@ if (!empty($rsp)) {
     // Rimuovo i mesi doppi
     array_unique($mesi_interventi);
 
-    // Ordino l'array per mese
-    ksort($mesi_interventi);
+    // Ordino l'array per anno
+	foreach($mesi_interventi as $key => &$data) {
+	  ksort($data);
+	}
 
     echo '<select class="superselect" id="select-intreventi-pianificare">';
 

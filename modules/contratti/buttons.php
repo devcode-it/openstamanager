d6bd4f29a7e0f8ff42e6bfccba52e3e09340162c
Luca <lucasalva87@users.noreply.github.com>
2018-11-22 11:34:07 +0100

    Fix permetto di fatturare il contratto solo se contiene righe e si trova in uno stato fatturabile

--- modules/contratti/buttons.php
+++ modules/contratti/buttons.php
@@ -2,18 +2,13 @@
 
 include_once __DIR__.'/../../core.php';
 
-$rs_documento = $dbo->fetchArray('SELECT * FROM co_righe_documenti WHERE idcontratto='.prepare($id_record));
-if (sizeof($rs_documento) > 0) {
-    echo '
-    <button type="button" class="btn btn-info" disabled>
-    <i class="fa fa-magic"></i> '.tr('Crea fattura').'...
-    </button>';
-} else {
-    echo "
-    <button type=\"button\" class=\"btn btn-info\" onclick=\"if( confirm('Creare una fattura per questo contratto?') ){fattura_da_contratto();}\">
-    <i class=\"fa fa-magic\"></i> ".tr('Crea fattura').'...
-    </button>';
-}
+$rs_documento = $dbo->fetchArray('SELECT * FROM co_righe_contratti WHERE idcontratto='.prepare($id_record));
+/* permetto di fatturare il contratto solo se contiene righe e si trova in uno stato fatturabile */
+echo "
+<button type=\"button\" class=\"btn btn-info\" ".((($record['fatturabile']) AND (sizeof($rs_documento)>0)) ? '' : 'disabled')." onclick=\"if( confirm('Creare una fattura per questo contratto?') ){fattura_da_contratto();}\">
+<i class=\"fa fa-magic\"></i> ".tr('Crea fattura').'...
+</button>';
+
 
 if ($record['rinnovabile']) {
     $rinnova = !empty($record['data_accettazione']) && !empty($record['data_conclusione']) && $record['data_accettazione'] != '0000-00-00' && $record['data_conclusione'] != '0000-00-00' &&  $record['pianificabile'];

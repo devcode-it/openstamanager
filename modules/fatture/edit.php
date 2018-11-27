fc867d115d47871655996a239002a1b6eb6df839
Luca <lucas@openstamanager.com>
2018-11-27 01:00:48 +0100

    Update edit.php

--- modules/fatture/edit.php
+++ modules/fatture/edit.php
@@ -265,16 +265,18 @@ if ($tipodoc == 'Fattura accompagnatoria di vendita') {
                 </div>
 
                 <div class="col-md-3">
-                    {[ "type": "select", "label": "'.tr('Vettore').'", "name": "idvettore", "values": "query=SELECT DISTINCT an_anagrafiche.idanagrafica AS id, an_anagrafiche.ragione_sociale AS descrizione FROM an_anagrafiche INNER JOIN an_tipianagrafiche_anagrafiche ON an_anagrafiche.idanagrafica=an_tipianagrafiche_anagrafiche.idanagrafica WHERE an_tipianagrafiche_anagrafiche.idtipoanagrafica=(SELECT idtipoanagrafica FROM an_tipianagrafiche WHERE descrizione=\'Vettore\') ORDER BY descrizione ASC", "value": "$idvettore$", "required": '.intval($record['idspedizione'] != 3).' ]}
+                    {[ "type": "select", "label": "'.tr('Vettore').'", "name": "idvettore", "values": "query=SELECT DISTINCT an_anagrafiche.idanagrafica AS id, an_anagrafiche.ragione_sociale AS descrizione FROM an_anagrafiche INNER JOIN an_tipianagrafiche_anagrafiche ON an_anagrafiche.idanagrafica=an_tipianagrafiche_anagrafiche.idanagrafica WHERE an_tipianagrafiche_anagrafiche.idtipoanagrafica=(SELECT idtipoanagrafica FROM an_tipianagrafiche WHERE descrizione=\'Vettore\') ORDER BY descrizione ASC", "value": "$idvettore$", "disabled": '.intval($record['idspedizione'] == 3).', "required": '.intval($record['idspedizione'] != 3).' ]}
                 </div>
 
                 <script>
                     $("#idspedizione").change( function(){
                         if ($(this).val() == 3) {
                             $("#idvettore").attr("required", false);
+                            $("#idvettore").attr("disabled", true);
                             $("#idvettore").parent().find("label").text("'.tr('Vettore').'")
                         }else{
                             $("#idvettore").attr("required", true);
+                            $("#idvettore").attr("disabled", false);
                             $("#idvettore").parent().find("label").text("'.tr('Vettore').'*")
                         }
                     });
@@ -338,7 +340,7 @@ if ($tipodoc == 'Fattura accompagnatoria di vendita') {
 
     echo '
                 <div class="col-md-3">
-                    {[ "type": "select", "label": "'.tr('Tipo Resa').'", "name": "tipo_resa", "values": '.json_encode($tipo_resa).', "readonly": '.intval($record['causale_desc'] != 'Reso').' ]}
+                    {[ "type": "select", "label": "'.tr('Tipo Resa').'", "name": "tipo_resa", "value":"$tipo_resa$", "values": '.json_encode($tipo_resa).', "readonly": '.intval($record['causale_desc'] != 'Reso').' ]}
                 </div>
 
             </div>

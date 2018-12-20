65d3fde2b4a8ccb7a6750705a14ae48a8542fb97
Fabio Lovato <fabiol@openstamanager.com>
2018-12-20 17:49:41 +0100

    Aggiunta la ragione sociale come variabile nell'invio mail fattura

--- modules/fatture/variables.php
+++ modules/fatture/variables.php
@@ -1,17 +1,18 @@
 <?php
 
-$r = $dbo->fetchOne('SELECT *,
-    (SELECT email FROM an_anagrafiche WHERE an_anagrafiche.idanagrafica=co_documenti.idanagrafica) AS email,
-    (SELECT pec FROM an_anagrafiche WHERE an_anagrafiche.idanagrafica=co_documenti.idanagrafica) AS pec,
+$r = $dbo->fetchOne('SELECT co_documenti.*,
+	an_anagrafiche.email,
+	an_anagrafiche.pec,
+	an_anagrafiche.ragione_sociale,
 	(SELECT pec FROM zz_smtps WHERE zz_smtps.id='.prepare($template['id_smtp']).') AS is_pec
-FROM co_documenti WHERE id='.prepare($id_record));
+FROM co_documenti INNER JOIN an_anagrafiche ON co_documenti.idanagrafica=an_anagrafiche.idanagrafica WHERE co_documenti.id='.prepare($id_record));
 
 // Variabili da sostituire
 return [
     'email' => $r['is_pec'] ? $r['pec'] : $r['email'],
     'id_anagrafica' => $r['idanagrafica'],
+	'ragione_sociale' => $r['ragione_sociale'],
     'numero' => empty($r['numero_esterno']) ? $r['numero'] : $r['numero_esterno'],
     'note' => $r['note'],
     'data' => Translator::dateToLocale($r['data']),
-    'id_anagrafica' => $r['idanagrafica'],
 ];

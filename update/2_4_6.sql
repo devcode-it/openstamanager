f08f3f1228104ea94d4445fcbddd81cc31b68975
Luca <lucasalva87@users.noreply.github.com>
2019-01-10 18:48:06 +0100

    Colonna tecnici per interventi

--- update/2_4_6.sql
+++ update/2_4_6.sql
@@ -1,1 +1,5 @@
+-- Aggiunto campo ritenutaenasarco
 ALTER TABLE `co_righe_documenti` ADD `ritenutaenasarco` DECIMAL(12,4) NOT NULL AFTER `ritenutaacconto`;
+
+-- Colonna tecnici per interventi
+INSERT INTO `zz_views` (`id`, `id_module`, `name`, `query`, `order`, `search`, `slow`, `format`, `search_inside`, `order_by`, `visible`, `summable`, `default`) VALUES (NULL, (SELECT `id` FROM `zz_modules` WHERE `name` = 'Interventi'), 'Tecnici', 'GROUP_CONCAT((SELECT DISTINCT(ragione_sociale) FROM an_anagrafiche WHERE idanagrafica = in_interventi_tecnici.idtecnico))', '14', '1', '0', '0', '', '', '1', '0', '0');

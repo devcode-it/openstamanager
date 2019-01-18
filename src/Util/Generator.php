ac90fbc237cb23eef3930d17c95bf1dc6caa2701
root <root@gestionale.firenetlab.local>
2019-01-18 16:49:27 +0100

    Fix per PHP7.3 dove preg_quote aggiunge il backslash anche al carattere #

--- src/Util/Generator.php
+++ src/Util/Generator.php
@@ -100,7 +100,7 @@ class Generator
         $values = array_column($replaces, 'regex');
 
         $pattern = preg_replace('/#{1,}/', '#', $pattern);
-        $pattern = preg_quote($pattern, '/');
+        $pattern = str_replace('\\#','#',preg_quote($pattern, '/'));
         $pattern = str_replace(array_keys($replaces), array_values($values), $pattern);
 
         // Individuazione dei valori

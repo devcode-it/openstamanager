595fbe01392582cac0551755ddfb74ba9eb3164b
Luca <lucasalva87@users.noreply.github.com>
2019-02-25 14:56:32 +0100

    Aggiunti campi id_conto mancanti

--- update/2_4_8.sql
+++ update/2_4_8.sql
@@ -1,1 +1,7 @@
 ALTER TABLE `zz_logs` CHANGE `username` `username` varchar(255);
+
+
+ALTER TABLE `dt_righe_ddt` ADD `id_conto` INT(11) NOT NULL AFTER `qta_evasa`;
+ALTER TABLE `co_righe_contratti` ADD `id_conto` INT(11) NOT NULL AFTER `qta_evasa`;
+ALTER TABLE `co_righe_preventivi` ADD `id_conto` INT(11) NOT NULL AFTER `qta_evasa`;
+ALTER TABLE `or_righe_ordini` ADD `id_conto` INT(11) NOT NULL AFTER `qta_evasa`;

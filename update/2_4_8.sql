5ac9e5d3633507bda0339f256118d5af80fd5544
Thomas Zilio <Dasc3er@users.noreply.github.com>
2019-02-26 21:40:23 +0100

    Fix query NumItem

--- update/2_4_8.sql
+++ update/2_4_8.sql
@@ -4,3 +4,9 @@ ALTER TABLE `zz_logs` CHANGE `username` `username` varchar(255);
 -- Aggiunta tipologia di documento Parcella
 INSERT INTO `co_tipidocumento` (`id`, `descrizione`, `dir`, `reversed`, `codice_tipo_documento_fe`) VALUES (NULL, 'Parcella', 'entrata', '0', 'TD06');
 INSERT INTO `co_tipidocumento` (`id`, `descrizione`, `dir`, `reversed`, `codice_tipo_documento_fe`) VALUES (NULL, 'Parcella', 'uscita', '0', 'TD06');
+
+-- Aggiunto codice cig e codice cup per contratti e interventi
+ALTER TABLE `co_contratti` ADD `num_item` VARCHAR(15) AFTER `id_documento_fe`;
+ALTER TABLE `in_interventi` ADD `num_item` VARCHAR(15) AFTER `id_documento_fe`;
+ALTER TABLE `or_ordini` ADD `num_item` VARCHAR(15) AFTER `id_documento_fe`;
+ALTER TABLE `co_preventivi` ADD `num_item` VARCHAR(15) AFTER `id_documento_fe`;
--- update/numitem.sql
+++
@@ -1,5 +0,0 @@
--- Aggiunto codice cig e codice cup per contratti e interventi
-ALTER TABLE `co_contratti` ADD `num_item` VARCHAR(15) AFTER `id_documento_fe`;
-ALTER TABLE `in_interventi` ADD `num_item` VARCHAR(15) AFTER `id_documento_fe`;
-ALTER TABLE `or_ordini` ADD `num_item` VARCHAR(15) AFTER `id_documento_fe`;
-ALTER TABLE `co_preventivi` ADD `num_item` VARCHAR(15) AFTER `id_documento_fe`;

fe532625f2470c2fe8bfdb6dc9d926dd33046fe1
Luca <lucasalva87@users.noreply.github.com>
2019-03-07 16:45:49 +0100

    Migliorie minori

---
+++ update/2_4_9.sql
@@ -0,0 +1,3 @@
+-- Escluso Art.74 ter D.P.R. 633/72
+INSERT INTO `co_iva` (`descrizione`, `percentuale`, `indetraibile`, `esente`, `codice_natura_fe`, `codice`, `default`) VALUES
+("Escluso Art.74 ter D.P.R. 633/72", 0, 0, 1, "N4", 0, 1);

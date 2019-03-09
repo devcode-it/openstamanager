dd3c3fe5866f398223a34efd6baf25346a00a64a
Luca <lucasalva87@users.noreply.github.com>
2019-03-09 13:24:00 +0100

    Aggiungo vista "Conto" per Fatture di acquisto (opzionale)

--- update/2_4_9.sql
+++ update/2_4_9.sql
@@ -1,3 +1,8 @@
 -- Escluso Art.74 ter D.P.R. 633/72
 INSERT INTO `co_iva` (`descrizione`, `percentuale`, `indetraibile`, `esente`, `codice_natura_fe`, `codice`, `default`) VALUES
 ("Escluso Art.74 ter D.P.R. 633/72", 0, 0, 1, "N4", NULL, 1);
+
+
+-- Aggiungo vista "Conto" per Fatture di acquisto (opzionale)
+INSERT INTO `zz_views` (`id`, `id_module`, `name`, `query`, `order`, `search`, `slow`, `format`, `search_inside`, `order_by`, `visible`, `summable`, `default`) VALUES
+(NULL, (SELECT id FROM zz_modules WHERE `name` = 'Fatture di acquisto') , 'Conto', '(SELECT GROUP_CONCAT(DISTINCT(co_pianodeiconti3.descrizione)) FROM co_righe_documenti INNER JOIN co_pianodeiconti3 ON co_pianodeiconti3.id = co_righe_documenti.idconto WHERE co_righe_documenti.iddocumento = co_documenti.id)', 10, 1, 0, 0, '', '', 0, 0, 1);

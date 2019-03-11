e56d32710976595ae78d9787715ac76b088bce75
Luca <lucasalva87@users.noreply.github.com>
2019-03-11 15:57:51 +0100

    Stato FE (Notifica esito)

--- update/2_4_9.sql
+++ update/2_4_9.sql
@@ -6,3 +6,7 @@ INSERT INTO `co_iva` (`descrizione`, `percentuale`, `indetraibile`, `esente`, `c
 -- Aggiungo vista "Conto" per Fatture di acquisto (opzionale)
 INSERT INTO `zz_views` (`id`, `id_module`, `name`, `query`, `order`, `search`, `slow`, `format`, `search_inside`, `order_by`, `visible`, `summable`, `default`) VALUES
 (NULL, (SELECT id FROM zz_modules WHERE `name` = 'Fatture di acquisto') , 'Conto', '(SELECT GROUP_CONCAT(DISTINCT(co_pianodeiconti3.descrizione)) FROM co_righe_documenti INNER JOIN co_pianodeiconti3 ON co_pianodeiconti3.id = co_righe_documenti.idconto WHERE co_righe_documenti.iddocumento = co_documenti.id)', 10, 1, 0, 0, '', '', 0, 0, 1);
+
+
+-- Stato FE (Notifica esito)
+INSERT INTO `fe_stati_documento` (`codice`, `descrizione`, `icon`) VALUES ('NE', 'Notifica esito', 'fa fa-check text-success');

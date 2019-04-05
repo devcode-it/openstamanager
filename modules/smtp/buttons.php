eb44f1d692c850f0ef394125f6f1e7f06cec8010
Thomas Zilio <Dasc3er@users.noreply.github.com>
2019-04-04 19:52:15 -0700

    Fix #292
    Aggiunto pulsante separato per il controllo sulle credenziali email.

---
+++ modules/smtp/buttons.php
@@ -0,0 +1,38 @@
+<?php
+
+echo '
+<button type="button" class="btn btn-primary" onclick="testAccount(this)">
+    <i class="fa fa-id-card-o"></i> '.tr('Controlla credenziali').'
+</button>
+
+<script>
+function testAccount(btn){
+    var restore = buttonLoading(btn);
+
+    $.ajax({
+        url: globals.rootdir + "/actions.php",
+        cache: false,
+        type: "POST",
+        data: {
+            id_module: globals.id_module,
+            id_record: globals.id_record,
+            op: "test",
+        },
+        success: function(data) {
+            buttonRestore(btn, restore);
+            
+            data = JSON.parse(data);
+            if(data.test){
+                swal("'.tr('Connessione SMTP riuscita').'", "'.tr("Connessione all'account SMTP completata con successo").'", "success");
+            }else{
+            swal("'.tr('Connessione SMTP fallita').'", "'.tr("Impossibile connettersi all'account SMTP").'", "error");
+            }
+        },
+        error: function(data) {
+            swal("'.tr('Errore').'", "'.tr('Errore durante il test').'", "error");
+
+            buttonRestore(btn, restore);
+        }
+    });
+}
+</script>';

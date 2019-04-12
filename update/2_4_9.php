38d56201c0307950ddd42d0ff76e0186ca59e238
Luca <lucasalva87@users.noreply.github.com>
2019-04-12 12:33:42 +0200

    Rimosso script fix ricalcolo scadenze pagamenti

--- update/2_4_9.php
+++ update/2_4_9.php
@@ -3,39 +3,6 @@
 include_once DOCROOT.'/modules/fatture/modutil.php';
 include_once DOCROOT.'/modules/interventi/modutil.php';
 
-// Fatture di vendita e acquisto
-$results = $dbo->fetchArray("SELECT co_documenti.id, co_statidocumento.descrizione AS stato_fattura
-FROM co_documenti 
-    INNER JOIN co_statidocumento ON co_documenti.idstatodocumento=co_statidocumento.id 
-WHERE co_statidocumento.descrizione IN ('Emessa', 'Parzialmente pagato', 'Pagato', 'Bozza', 'Annullata')");
-
-foreach ($results as $result) {
-    $scadenze = $dbo->fetchArray('SELECT id, da_pagare, pagato, scadenza FROM co_scadenziario WHERE iddocumento = '.prepare($result['id']));
-
-    // Se esiste la scadenza la elimino
-    if (!empty($scadenze)) {
-        elimina_scadenza($result['id']);
-    }
-
-    $is_pagato = null;
-    if ($result['stato_fattura'] == 'Pagato') {
-        $is_pagato = true;
-    } elseif (in_array($result['stato_fattura'], ['Emessa', 'Parzialmente pagato'])) {
-        $is_pagato = false;
-    }
-
-    // Aggiungo la scadenza e la segno eventualmente come pagata
-    if (isset($is_pagato)) {
-        aggiungi_scadenza($result['id'], null, $is_pagato);
-    }
-
-    if (!empty($scadenze) && $result['stato_fattura'] == 'Parzialmente pagato') {
-        foreach ($scadenze as $scadenza) {
-            $dbo->query('UPDATE co_scadenziario SET pagato = '.prepare($scadenza['pagato']).' WHERE scadenza = '.prepare($scadenza['scadenza']).' AND iddocumento = '.prepare($result['id']));
-        }
-    }
-}
-
 // Aggiornamento sconti incodizionati per Interventi
 $id_iva = setting('Iva predefinita');
 $iva = $dbo->fetchOne('SELECT * FROM co_iva WHERE id='.prepare($id_iva));

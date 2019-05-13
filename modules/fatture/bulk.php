4cf9856c81af8ef45a2170eb6c3a323ce7fc7c37
Luca <lucasalva87@users.noreply.github.com>
2019-05-13 17:01:43 +0200

    Fix minori

--- modules/fatture/bulk.php
+++ modules/fatture/bulk.php
@@ -51,18 +51,16 @@ switch (post('op')) {
         break;
 
     case 'delete-bulk':
-        if (App::debug()) {
-            foreach ($id_records as $id) {
-                $dbo->query('DELETE FROM co_documenti  WHERE id = '.prepare($id).Modules::getAdditionalsQuery($id_module));
-                $dbo->query('DELETE FROM co_righe_documenti WHERE iddocumento='.prepare($id).Modules::getAdditionalsQuery($id_module));
-                $dbo->query('DELETE FROM co_scadenziario WHERE iddocumento='.prepare($id).Modules::getAdditionalsQuery($id_module));
-                $dbo->query('DELETE FROM mg_movimenti WHERE iddocumento='.prepare($id).Modules::getAdditionalsQuery($id_module));
-            }
+      
+		foreach ($id_records as $id) {
+			$dbo->query('DELETE FROM co_documenti  WHERE id = '.prepare($id).Modules::getAdditionalsQuery($id_module));
+			$dbo->query('DELETE FROM co_righe_documenti WHERE iddocumento='.prepare($id).Modules::getAdditionalsQuery($id_module));
+			$dbo->query('DELETE FROM co_scadenziario WHERE iddocumento='.prepare($id).Modules::getAdditionalsQuery($id_module));
+			$dbo->query('DELETE FROM mg_movimenti WHERE iddocumento='.prepare($id).Modules::getAdditionalsQuery($id_module));
+		}
 
-            flash()->info(tr('Fatture eliminate!'));
-        } else {
-            flash()->warning(tr('Procedura in fase di sviluppo. Nessuna modifica apportata.'));
-        }
+		flash()->info(tr('Fatture eliminate!'));
+        
         break;
 
     case 'export-xml-bulk':
@@ -283,11 +281,15 @@ switch (post('op')) {
         break;
 }
 
-$bulk = [
-    'delete-bulk' => tr('Elimina selezionati'),
-];
+if (App::debug()) {
+	
+	$operations = [
+		'delete-bulk' => tr('Elimina selezionati'),
+	];
 
-$bulk['registra-contabile'] = [
+}
+
+$operations['registra-contabile'] = [
     'text' => tr('Registra contabile pagamento'),
     'data' => [
         'msg' => tr('Vuoi aggiungere un movimento contabile per le fatture selezionate? (le fatture dovranno essere in stato emessa altrimenti non verranno elaborate)'),
@@ -298,7 +300,7 @@ $bulk['registra-contabile'] = [
 ];
 
 if ($module->name == 'Fatture di vendita') {
-    $bulk['export-bulk'] = [
+    $operations['export-bulk'] = [
         'text' => tr('Esporta stampe'),
         'data' => [
             'msg' => tr('Vuoi davvero esportare tutte le stampe in un archivio?'),
@@ -309,7 +311,7 @@ if ($module->name == 'Fatture di vendita') {
     ];
 }
 
-$bulk['export-xml-bulk'] = [
+$operations['export-xml-bulk'] = [
     'text' => tr('Esporta XML'),
     'data' => [
         'msg' => tr('Vuoi davvero esportare tutte le fatture elettroniche in un archivio?'),
@@ -319,4 +321,4 @@ $bulk['export-xml-bulk'] = [
     ],
 ];
 
-return $bulk;
+return $operations;

ed14acc7010c1b83977944d85ea0828a74b9387d
Thomas Zilio <Dasc3er@users.noreply.github.com>
2019-05-20 17:21:09 -0700

    Fix totale fatture di acquisto

--- update/2_4_10.sql
+++ update/2_4_10.sql
@@ -1,2 +1,3 @@
--- Fix colonna Totale per fatture di vendita
+-- Fix colonna Totale per Fatture
 UPDATE `zz_views` SET `query` = '(SELECT SUM(subtotale - sconto + iva + rivalsainps - ritenutaacconto) FROM co_righe_documenti WHERE co_righe_documenti.iddocumento=co_documenti.id GROUP BY iddocumento) + iva_rivalsainps' WHERE `zz_views`.`id_module` = (SELECT `id` FROM `zz_modules` WHERE `name` = 'Fatture di vendita') AND name = 'Totale';
+UPDATE `zz_views` SET `query` = '(SELECT SUM(subtotale - sconto + iva + rivalsainps - ritenutaacconto) FROM co_righe_documenti WHERE co_righe_documenti.iddocumento=co_documenti.id GROUP BY iddocumento) + iva_rivalsainps' WHERE `zz_views`.`id_module` = (SELECT `id` FROM `zz_modules` WHERE `name` = 'Fatture di acquisto') AND name = 'Totale';

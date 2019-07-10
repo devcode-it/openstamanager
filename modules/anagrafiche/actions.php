95a6a8cd78430656a7adc2af561db6639fb95a55
loviuz <loviuz@gmail.com>
2019-07-10 09:57:09 +0200

    Fix salvataggio idtipointervento_default in anagrafiche

--- modules/anagrafiche/actions.php
+++ modules/anagrafiche/actions.php
@@ -73,7 +73,7 @@ switch (post('op')) {
         $anagrafica->n_alboartigiani = post('n_alboartigiani');
         $anagrafica->foro_competenza = post('foro_competenza');
         $anagrafica->colore = post('colore');
-        $anagrafica->idtipointervento_default = post('idtipointervento_default');
+        $anagrafica->idtipointervento_default = post('idtipointervento_default') ?: null;
         $anagrafica->id_ritenuta_acconto_acquisti = post('id_ritenuta_acconto_acquisti');
         $anagrafica->id_ritenuta_acconto_vendite = post('id_ritenuta_acconto_vendite');
         $anagrafica->split_payment = post('split_payment');

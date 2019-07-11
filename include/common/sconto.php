86f2a10040ab315dba4adb4705db1093c3ebb7b7
Thomas Zilio <Dasc3er@users.noreply.github.com>
2019-07-11 17:28:53 +0200

    Fix modifica sconti percentuali

--- include/common/sconto.php
+++ include/common/sconto.php
@@ -8,20 +8,24 @@ echo App::internalLoad('conti.php', $result, $options);
 
 // Sconto percentuale
 echo '
-    <div class="row">
+    <div class="row">';
+
+if ($options['action'] == 'add') {
+    echo '
         <div class="col-md-4">
             {[ "type": "number", "label": "'.tr('Sconto/maggiorazione percentuale').'", "name": "sconto_percentuale", "icon-after": "%", "help": "'.tr('Il valore positivo indica uno sconto: per applicare una maggiorazione inserire un valore negativo').'" ]}
         </div>';
+}
 
 // Sconto unitario
 echo '
-        <div class="col-md-4">
+        <div class="col-md-'.($options['action'] == 'add' ? 4 : 6).'">
             {[ "type": "number", "label": "'.tr('Sconto/maggiorazione unitario').'", "name": "sconto_unitario", "value": "'.$result['sconto_unitario'].'", "icon-after": "'.currency().'", "help": "'.tr('Il valore positivo indica uno sconto: per applicare una maggiorazione inserire un valore negativo').'" ]}
         </div>';
 
 // Iva
 echo '
-        <div class="col-md-4">
+        <div class="col-md-'.($options['action'] == 'add' ? 4 : 6).'">
             {[ "type": "select", "label": "'.tr('Iva').'", "name": "idiva", "required": 1, "value": "'.$result['idiva'].'", "ajax-source": "iva" ]}
         </div>
     </div>';

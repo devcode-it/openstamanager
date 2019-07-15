c4695e6b717ed5c9130fbac02b47e6fc72f4d23e
Thomas Zilio <Dasc3er@users.noreply.github.com>
2019-07-15 12:56:17 +0200

    Messaggio sui permessi

--- pdfgen.php
+++ pdfgen.php
@@ -14,4 +14,20 @@ if (!empty($ptype)) {
     $id_record = !empty($id_record) ? $id_record : get($print[0]['previous']);
 }
 
-Prints::render($id_print, $id_record, $directory);
+$result = Prints::render($id_print, $id_record, $directory);
+
+if(empty($result)){
+    echo '
+        <div class="text-center">
+    		<h3 class="text-muted">
+    		    <i class="fa fa-question-circle"></i> '.tr('Record non trovato').'
+                <br><br>
+                <small class="help-block">'.tr('Stai cercando di accedere ad un record eliminato o non presente').'.</small>
+            </h3>
+            <br>
+
+            <a class="btn btn-default" href="'.ROOTDIR.'/index.php">
+                <i class="fa fa-chevron-left"></i> '.tr('Indietro').'
+            </a>
+        </div>';
+}

fe852e5387d7d6143730e0fc2f5272a2a62e429b
loviuz <loviuz@gmail.com>
2019-08-02 18:18:26 +0200

    Aggiornamento indirizzo PEC dello SDI

--- update/2_4_11.sql
+++ update/2_4_11.sql
@@ -292,3 +292,6 @@ ALTER TABLE `zz_files` ADD `created_by` INT(11) AFTER `id_record`, ADD FOREIGN K
 
 -- Aggiunto riferimento allo Scadenzario nella Prima Nota
 ALTER TABLE `co_movimenti` ADD `id_scadenza` INT(11) AFTER `iddocumento`, ADD FOREIGN KEY (`id_scadenza`) REFERENCES `co_scadenziario`(`id`) ON DELETE CASCADE, ADD `is_insoluto` BOOLEAN NOT NULL DEFAULT FALSE AFTER `id_scadenza`;
+
+-- Aggiornamento indirizzo email SDI
+UPDATE `zz_emails` SET `cc` = 'sdi52@pec.fatturapa.it' WHERE `name` = 'PEC';

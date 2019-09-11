2a947a0c1dc2738e5ebac3c26c175d28216e737a
Thomas Zilio <Dasc3er@users.noreply.github.com>
2019-09-11 16:50:42 +0200

    Miglioramento giacenze e qta progressiva

--- modules/articoli/plugins/articoli.movimenti.php
+++ modules/articoli/plugins/articoli.movimenti.php
@@ -3,7 +3,6 @@
 include_once __DIR__.'/../../../core.php';
 
 // Movimentazione degli articoli
-
 echo '
 <div class="box">
     <div class="box-header with-border">
@@ -30,9 +29,8 @@ if (empty($_GET['show_all1'])) {
     $query .= ' LIMIT 0, 20';
 }
 
-$rs2 = $dbo->fetchArray($query);
-
-if (!empty($rs2)) {
+$movimenti = $dbo->fetchArray($query);
+if (!empty($movimenti)) {
     if (empty($_GET['show_all1'])) {
         echo '
         <p><a href="'.$rootdir.'/editor.php?id_module='.$id_module.'&id_record='.$id_record.'&show_all1=1#tab_'.$id_plugin.'">[ '.tr('Mostra tutti i movimenti').' ]</a></p>';
@@ -44,42 +42,63 @@ if (!empty($rs2)) {
     echo '
         <table class="table table-striped table-condensed table-bordered">
             <tr>
-                <th >'.tr('Q.tà').'</th>
-                <th >'.tr('Causale').'</th>
-                <th  >'.tr('Data').'</th>
+                <th class="text-center">'.tr('Q.tà').'</th>
+                <th class="text-center">'.tr('Q.tà progressiva').'</th>
+                <th>'.tr('Causale').'</th>
+                <th class="text-center">'.tr('Data').'</th>
                 <th class="text-center" width="7%">#</th>
             </tr>';
-    foreach ($rs2 as $r) {
+
+    foreach ($movimenti as $i => $movimento) {
+        // Quantità progressiva
+        if ($i == 0) {
+            $movimento['progressivo_finale'] = $articolo->qta;
+        } else {
+            $movimento['progressivo_finale'] = $movimenti[$i - 1]['progressivo_iniziale'];
+        }
+        $movimento['progressivo_iniziale'] = $movimento['progressivo_finale'] - intval($movimento['qta']);
+
+        $movimenti[$i]['progressivo_iniziale'] = $movimento['progressivo_iniziale'];
+        $movimenti[$i]['progressivo_finale'] = $movimento['progressivo_finale'];
+
         // Quantità
         echo '
             <tr>
-                <td class="text-right">'.Translator::numberToLocale($r['qta'], 'qta').' '.$record['um'].'</td>';
+                <td class="text-center">
+                    '.numberFormat($movimento['qta'], 'qta').' '.$record['um'].'
+                </td>
+                
+                <td class="text-center">
+                    '.numberFormat($movimento['progressivo_iniziale'], 'qta').' '.$record['um'].'
+                    <i class="fa fa-arrow-circle-right"></i>
+                    '.numberFormat($movimento['progressivo_finale'], 'qta').' '.$record['um'].'
+                </td>';
 
         // Causale
-        $dir = ($r['qta'] < 0) ? 'vendita' : 'acquisto';
+        $dir = ($movimento['qta'] < 0) ? 'vendita' : 'acquisto';
 
-        if (!empty($r['iddocumento'])) {
-            $dir = $dbo->fetchArray('SELECT dir FROM co_tipidocumento WHERE id = (SELECT idtipodocumento FROM co_documenti WHERE id = '.prepare($r['iddocumento']).')')[0]['dir'] == 'entrata' ? 'vendita' : 'acquisto';
+        if (!empty($movimento['iddocumento'])) {
+            $dir = $dbo->fetchArray('SELECT dir FROM co_tipidocumento WHERE id = (SELECT idtipodocumento FROM co_documenti WHERE id = '.prepare($movimento['iddocumento']).')')[0]['dir'] == 'entrata' ? 'vendita' : 'acquisto';
         }
 
         echo '
-                <td>'.$r['movimento'].'
-				'.((!empty($r['idintervento'])) ? Modules::link('Interventi', $r['idintervento']) : '').'
-				'.((!empty($r['idddt'])) ? (Modules::link('DDt di '.$dir, $r['idddt'], null, null, (intval($database->fetchOne('SELECT * FROM `dt_ddt` WHERE `id` ='.prepare($r['idddt'])))) ? '' : 'class="disabled"')) : '').'
-				'.((!empty($r['iddocumento'])) ? (Modules::link('Fatture di '.$dir, $r['iddocumento'], null, null, (intval($database->fetchOne('SELECT * FROM `co_documenti` WHERE `id` ='.prepare($r['iddocumento'])))) ? '' : 'class="disabled"')) : '').'
+                <td>'.$movimento['movimento'].'
+				'.((!empty($movimento['idintervento'])) ? Modules::link('Interventi', $movimento['idintervento']) : '').'
+				'.((!empty($movimento['idddt'])) ? (Modules::link('DDt di '.$dir, $movimento['idddt'], null, null, (intval($database->fetchOne('SELECT * FROM `dt_ddt` WHERE `id` ='.prepare($movimento['idddt'])))) ? '' : 'class="disabled"')) : '').'
+				'.((!empty($movimento['iddocumento'])) ? (Modules::link('Fatture di '.$dir, $movimento['iddocumento'], null, null, (intval($database->fetchOne('SELECT * FROM `co_documenti` WHERE `id` ='.prepare($movimento['iddocumento'])))) ? '' : 'class="disabled"')) : '').'
 				</td>';
 
         // Data
         echo '
-                <td class="text-center" >'.Translator::dateToLocale($r['data']).' <span  class=\'tip\' title=\''.tr('Data del movimento: ').Translator::timestampToLocale($r['created_at']).'\' ><i class="fa fa-question-circle-o"></i></span> </td>';
+                <td class="text-center" >'.Translator::dateToLocale($movimento['data']).' <span  class=\'tip\' title=\''.tr('Data del movimento: ').Translator::timestampToLocale($movimento['created_at']).'\' ><i class="fa fa-question-circle-o"></i></span> </td>';
 
         // Operazioni
         echo '
                 <td class="text-center">';
 
-        if (Auth::admin() && $r['manuale'] == '1') {
+        if (Auth::admin() && $movimento['manuale'] == '1') {
             echo '
-                    <a class="btn btn-danger btn-sm ask" data-backto="record-edit" data-op="delmovimento" data-idmovimento="'.$r['id'].'">
+                    <a class="btn btn-danger btn-sm ask" data-backto="record-edit" data-op="delmovimento" data-idmovimento="'.$movimento['id'].'">
                         <i class="fa fa-trash"></i>
                     </a>';
         }
@@ -88,13 +107,14 @@ if (!empty($rs2)) {
                 </td>
             </tr>';
     }
+
     echo '
         </table>';
 } else {
     echo '
 	<div class="alert alert-info">
 		<i class="fa fa-info-circle"></i>
-		'.tr('Questo articolo non è ancora stato movimentato', []).'.
+		'.tr('Questo articolo non è ancora stato movimentato').'.
 	</div>';
 }
 

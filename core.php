171948612c9c57d25d82bfbc415f85ea7488175e
Thomas Zilio <Dasc3er@users.noreply.github.com>
2019-09-30 10:41:49 +0200

    Namespace per pesonalizzazione
    Namespace  aggiuntivo Original per favorire la compaitibilità delle personalizzazioni di classi con le nuove versioni.

--- core.php
+++ core.php
@@ -28,6 +28,9 @@ $namespaces = require_once __DIR__.'/config/namespaces.php';
 foreach ($namespaces as $path => $namespace) {
     $loader->addPsr4($namespace.'\\', __DIR__.'/'.$path.'/custom/src');
     $loader->addPsr4($namespace.'\\', __DIR__.'/'.$path.'/src');
+
+    // Namespace aggiuntivo per le personalizzazioni
+    $loader->addPsr4($namespace.'\\Original\\', __DIR__.'/'.$path.'/src');
 }
 
 // Individuazione dei percorsi di base

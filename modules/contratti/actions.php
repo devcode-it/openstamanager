99d1a475cfbe5f495a04ba9ddd6743ccfd28f04b
Thomas Zilio <Dasc3er@users.noreply.github.com>
2019-11-12 16:48:06 +0100

    Correzioni #673

--- modules/contratti/actions.php
+++ modules/contratti/actions.php
@@ -273,88 +273,83 @@ switch (post('op')) {
 
     // Rinnovo contratto
     case 'renew':
-        $rs = $dbo->fetchArray('SELECT *, DATEDIFF(data_conclusione, data_accettazione) AS giorni FROM co_contratti WHERE id='.prepare($id_record));
+        $giorni = $contratto->data_conclusione->diffInDays($contratto->data_accettazione);
 
-        if (sizeof($rs) == 1) {
-            // Verifico se il rinnovo contratto è un numero accettabile con la differenza di data inizio e data fine
-            if ($rs[0]['giorni'] > 0 && $rs[0]['giorni'] < 365 * 10) {
-                $giorni_add = $rs[0]['giorni'];
-            } else {
-                $giorni_add = 0;
-            }
-
-            // Calcolo numero successivo contratti
-            $rs2 = $dbo->fetchArray('SELECT MAX(CAST(numero AS UNSIGNED)) AS maxn FROM co_contratti');
-            $numero = $rs2[0]['maxn'] + 1;
-
-            if ($dbo->query('INSERT INTO co_contratti(numero, nome, idagente, data_bozza, data_accettazione, data_rifiuto, data_conclusione, rinnovabile, giorni_preavviso_rinnovo, budget, descrizione, idstato, idreferente, validita, esclusioni, idanagrafica, idpagamento, costo_diritto_chiamata, ore_lavoro, costo_orario, costo_km, idcontratto_prev) VALUES('.prepare($numero).', '.prepare($rs[0]['nome']).', '.prepare($rs[0]['idagente']).', NOW(), '.prepare(date('Y-m-d', strtotime($rs[0]['data_conclusione'].' +1 day'))).', "", '.prepare(date('Y-m-d', strtotime($rs[0]['data_conclusione'].' +'.$giorni_add.' day'))).', '.prepare($rs[0]['rinnovabile']).', '.prepare($rs[0]['giorni_preavviso_rinnovo']).', '.prepare($rs[0]['budget']).', '.prepare($rs[0]['descrizione']).', '.prepare($rs[0]['idstato']).', '.prepare($rs[0]['idreferente']).', '.prepare($rs[0]['validita']).', '.prepare($rs[0]['esclusioni']).', '.prepare($rs[0]['idanagrafica']).', '.prepare($rs[0]['idpagamento']).', '.prepare($rs[0]['costo_diritto_chiamata']).', '.prepare($rs[0]['ore_lavoro']).', '.prepare($rs[0]['costo_orario']).', '.prepare($rs[0]['costo_km']).', '.prepare($id_record).')')) {
-                $new_idcontratto = $dbo->lastInsertedID();
-
-                $dbo->query('INSERT INTO co_contratti_tipiintervento(idcontratto, idtipointervento, costo_ore, costo_km, costo_dirittochiamata, costo_ore_tecnico, costo_km_tecnico, costo_dirittochiamata_tecnico) SELECT '.prepare($new_idcontratto).', idtipointervento, costo_ore, costo_km, costo_dirittochiamata, costo_ore_tecnico, costo_km_tecnico, costo_dirittochiamata_tecnico FROM co_contratti_tipiintervento AS z WHERE idcontratto='.prepare($id_record));
-
-                // Replico le righe del contratto
-                $rs = $dbo->fetchArray('SELECT * FROM co_righe_contratti WHERE idcontratto='.prepare($id_record));
-
-                for ($i = 0; $i < sizeof($rs); ++$i) {
-                    unset($rs[$i]['id']);
-                    unset($rs[$i]['created_at']);
-                    unset($rs[$i]['updated_at']);
-                    unset($rs[$i]['qta_evasa']);
-                    $rs[$i]['idcontratto'] = $new_idcontratto;
-
-                    $dbo->insert('co_righe_contratti', $rs);
-                }
-
-                // Replicazione degli impianti
-                $impianti = $dbo->fetchArray('SELECT idimpianto FROM my_impianti_contratti WHERE idcontratto='.prepare($id_record));
-                $dbo->sync('my_impianti_contratti', ['idcontratto' => $new_idcontratto], ['idimpianto' => array_column($impianti, 'idimpianto')]);
-
-                // Replicazione dei promemoria
-                $promemoria = $dbo->fetchArray('SELECT * FROM co_promemoria WHERE idcontratto='.prepare($id_record));
-                foreach ($promemoria as $p) {
-                    $dbo->insert('co_promemoria', [
-                        'idcontratto' => $new_idcontratto,
-                        'data_richiesta' => date('Y-m-d', strtotime($p['data_richiesta'].' +'.$giorni_add.' day')),
-                        'idtipointervento' => $p['idtipointervento'],
-                        'richiesta' => $p['richiesta'],
-                        'idimpianti' => $p['idimpianti'],
-                    ]);
-                    $id_promemoria = $dbo->lastInsertedID();
-
-                    // Copia degli articoli
-                    $dbo->query('INSERT INTO co_promemoria_articoli(idarticolo, id_promemoria, idimpianto, descrizione, prezzo_vendita, prezzo_acquisto, sconto, sconto_unitario, tipo_sconto, idiva, desc_iva, iva, qta, um, abilita_serial) SELECT idarticolo, :id_new, idimpianto, descrizione, prezzo_vendita, prezzo_acquisto, sconto, sconto_unitario, tipo_sconto, idiva, desc_iva, iva, qta, um, abilita_serial FROM co_promemoria_articoli AS z WHERE id_promemoria = :id_old', [
-                        ':id_new' => $id_promemoria,
-                        ':id_old' => $p['id'],
-                    ]);
-
-                    // Copia delle righe
-                    $dbo->query('INSERT INTO co_promemoria_righe(id_promemoria, descrizione, qta, um, prezzo_vendita, prezzo_acquisto, idiva, desc_iva, iva, sconto, sconto_unitario, tipo_sconto) SELECT :id_new, descrizione, qta, um, prezzo_vendita, prezzo_acquisto, idiva, desc_iva, iva, sconto, sconto_unitario, tipo_sconto FROM co_promemoria_righe AS z WHERE id_promemoria = :id_old', [
-                        ':id_new' => $id_promemoria,
-                        ':id_old' => $p['id'],
-                    ]);
-
-                    // Copia degli allegati
-                    Uploads::copy([
-                        'id_module' => $id_module,
-                        'id_plugin' => Plugins::get('Pianificazione interventi')['id'],
-                        'id_record' => $p['id'],
-                    ], [
-                        'id_module' => $id_module,
-                        'id_plugin' => Plugins::get('Pianificazione interventi')['id'],
-                        'id_record' => $id_promemoria,
-                    ]);
-                }
-
-                // Cambio stato precedente contratto in concluso (non più pianificabile)
-                $dbo->query('UPDATE `co_contratti` SET `rinnovabile`= 0, `idstato`= (SELECT id FROM co_staticontratti WHERE is_pianificabile = 0 AND is_fatturabile = 1 AND descrizione = \'Concluso\')  WHERE `id` = '.prepare($id_record));
-
-                flash()->info(tr('Contratto rinnovato!'));
-
-                $id_record = $new_idcontratto;
-            } else {
-                flash()->error(tr('Errore durante il rinnovo del contratto!'));
-            }
+        // Verifico se il rinnovo contratto è un numero accettabile con la differenza di data inizio e data fine
+        if ($giorni < 0 || $giorni > 365 * 10) {
+            $giorni = 0;
         }
+
+        $new_contratto = $contratto->replicate();
+        $new_contratto->numero = Contratto::getNextNumero();
+
+        $new_contratto->idcontratto_prev = $contratto->id;
+        $new_contratto->data_accettazione = $contratto->data_conclusione->addDays(1);
+        $new_contratto->data_conclusione = $new_contratto->data_accettazione->addDays($giorni);
+        $new_contratto->save();
+        $new_idcontratto = $new_contratto->id;
+
+        // Correzioni dei prezzi per gli interventi
+        $dbo->query('DELETE FROM co_contratti_tipiintervento WHERE idcontratto='.prepare($new_idcontratto));
+        $dbo->query('INSERT INTO co_contratti_tipiintervento(idcontratto, idtipointervento, costo_ore, costo_km, costo_dirittochiamata, costo_ore_tecnico, costo_km_tecnico, costo_dirittochiamata_tecnico) SELECT '.prepare($new_idcontratto).', idtipointervento, costo_ore, costo_km, costo_dirittochiamata, costo_ore_tecnico, costo_km_tecnico, costo_dirittochiamata_tecnico FROM co_contratti_tipiintervento AS z WHERE idcontratto='.prepare($id_record));
+        $new_contratto->save();
+
+        // Replico le righe del contratto
+        $righe = $contratto->getRighe();
+        foreach ($righe as $riga) {
+            $new_riga = $riga->replicate();
+            $new_riga->qta_evasa = 0;
+            $new_riga->idcontratto = $new_contratto->id;
+
+            $new_riga->save();
+        }
+
+        // Replicazione degli impianti
+        $impianti = $dbo->fetchArray('SELECT idimpianto FROM my_impianti_contratti WHERE idcontratto='.prepare($id_record));
+        $dbo->sync('my_impianti_contratti', ['idcontratto' => $new_idcontratto], ['idimpianto' => array_column($impianti, 'idimpianto')]);
+
+        // Replicazione dei promemoria
+        $promemoria = $dbo->fetchArray('SELECT * FROM co_promemoria WHERE idcontratto='.prepare($id_record));
+        foreach ($promemoria as $p) {
+            $dbo->insert('co_promemoria', [
+                'idcontratto' => $new_idcontratto,
+                'data_richiesta' => date('Y-m-d', strtotime($p['data_richiesta'].' +'.$giorni.' day')),
+                'idtipointervento' => $p['idtipointervento'],
+                'richiesta' => $p['richiesta'],
+                'idimpianti' => $p['idimpianti'],
+            ]);
+            $id_promemoria = $dbo->lastInsertedID();
+
+            // Copia degli articoli
+            $dbo->query('INSERT INTO co_promemoria_articoli(idarticolo, id_promemoria, idimpianto, descrizione, prezzo_vendita, prezzo_acquisto, sconto, sconto_unitario, tipo_sconto, idiva, desc_iva, iva, qta, um, abilita_serial) SELECT idarticolo, :id_new, idimpianto, descrizione, prezzo_vendita, prezzo_acquisto, sconto, sconto_unitario, tipo_sconto, idiva, desc_iva, iva, qta, um, abilita_serial FROM co_promemoria_articoli AS z WHERE id_promemoria = :id_old', [
+                ':id_new' => $id_promemoria,
+                ':id_old' => $p['id'],
+            ]);
+
+            // Copia delle righe
+            $dbo->query('INSERT INTO co_promemoria_righe(id_promemoria, descrizione, qta, um, prezzo_vendita, prezzo_acquisto, idiva, desc_iva, iva, sconto, sconto_unitario, tipo_sconto) SELECT :id_new, descrizione, qta, um, prezzo_vendita, prezzo_acquisto, idiva, desc_iva, iva, sconto, sconto_unitario, tipo_sconto FROM co_promemoria_righe AS z WHERE id_promemoria = :id_old', [
+                ':id_new' => $id_promemoria,
+                ':id_old' => $p['id'],
+            ]);
+
+            // Copia degli allegati
+            Uploads::copy([
+                'id_module' => $id_module,
+                'id_plugin' => Plugins::get('Pianificazione interventi')['id'],
+                'id_record' => $p['id'],
+            ], [
+                'id_module' => $id_module,
+                'id_plugin' => Plugins::get('Pianificazione interventi')['id'],
+                'id_record' => $id_promemoria,
+            ]);
+        }
+
+        // Cambio stato precedente contratto in concluso (non più pianificabile)
+        $dbo->query('UPDATE `co_contratti` SET `rinnovabile`= 0, `idstato`= (SELECT id FROM co_staticontratti WHERE is_pianificabile = 0 AND is_fatturabile = 1 AND descrizione = \'Concluso\')  WHERE `id` = '.prepare($id_record));
+
+        flash()->info(tr('Contratto rinnovato!'));
+
+        $id_record = $new_idcontratto;
 
         break;
 

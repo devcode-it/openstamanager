6525786fbf898bf58247a5fd4a63abfaa6386b1c
Thomas Zilio <Dasc3er@users.noreply.github.com>
2019-11-22 16:54:56 +0100

    Correzioni visualizzazione mastrini

--- modules/partitario/dettagli_conto.php
+++ modules/partitario/dettagli_conto.php
@@ -2,13 +2,21 @@
 
 include_once __DIR__.'/../../core.php';
 
+$prima_nota = Modules::get('Prima nota');
+
 $id_conto = get('id_conto');
 
 // Calcolo totale conto da elenco movimenti di questo conto
-$query = 'SELECT co_movimenti.*, dir FROM co_movimenti
-                LEFT OUTER JOIN co_documenti ON co_movimenti.iddocumento = co_documenti.id
-                LEFT OUTER JOIN co_tipidocumento ON co_documenti.idtipodocumento = co_tipidocumento.id
-            WHERE co_movimenti.idconto='.prepare($id_conto).' AND co_movimenti.data >= '.prepare($_SESSION['period_start']).' AND co_movimenti.data <= '.prepare($_SESSION['period_end']).' ORDER BY co_movimenti.data DESC';
+$query = 'SELECT co_movimenti.*,
+    SUM(totale) AS totale,
+    dir FROM co_movimenti
+LEFT OUTER JOIN co_documenti ON co_movimenti.iddocumento = co_documenti.id
+LEFT OUTER JOIN co_tipidocumento ON co_documenti.idtipodocumento = co_tipidocumento.id
+WHERE co_movimenti.idconto='.prepare($id_conto).' AND
+    co_movimenti.data >= '.prepare($_SESSION['period_start']).' AND
+    co_movimenti.data <= '.prepare($_SESSION['period_end']).'
+GROUP BY co_movimenti.idmastrino
+ORDER BY co_movimenti.data DESC';
 $movimenti = $dbo->fetchArray($query);
 
 if (!empty($movimenti)) {
@@ -30,8 +38,7 @@ if (!empty($movimenti)) {
         if (!empty($movimento['iddocumento'])) {
             $modulo_fattura = ($movimento['dir'] == 'entrata') ? Modules::get('Fatture di vendita')['id'] : Modules::get('Fatture di acquisto')['id'];
 
-            echo '
-            <a class="clickable" onclick="open_movimento('.$movimento['id'].', '.$modulo_fattura.')">'.$movimento['descrizione'].'</a>';
+            echo Modules::link($prima_nota->id, $movimento['idmastrino'], $movimento['descrizione']);
         } else {
             echo '
             <span>'.$movimento['descrizione'].'</span>';
@@ -72,8 +79,8 @@ if (!empty($movimenti)) {
 </table>
 
 <script>
-function open_movimento(id_movimento, id_module){
-    launch_modal("'.tr('Dettagli movimento').'", "'.$structure->fileurl('dettagli_movimento.php').'?id_movimento=" + id_movimento + "&id_module=" + id_module);
+function open_movimento(id_mastrino, id_module){
+    launch_modal("'.tr('Dettagli movimento').'", "'.$structure->fileurl('dettagli_movimento.php').'?id_mastrino=" + id_mastrino + "&id_module=" + id_module);
 }
 </script>';
 } else {

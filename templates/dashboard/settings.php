b32a08d196516a3d615a0a60b962a4c60221616d
Thomas Zilio <Dasc3er@users.noreply.github.com>
2019-11-22 16:30:25 +0100

    Fix minore

--- templates/dashboard/settings.php
+++ templates/dashboard/settings.php
@@ -1,6 +1,5 @@
 <?php
 
-$orientation = (isset($_SESSION['settings']['orientation'])) ? $_SESSION['settings']['orientation'] : 'L';
 $format = (isset($_SESSION['dashboard']['format'])) ? $_SESSION['dashboard']['format'] : 'A4';
 $orientation = (isset($_SESSION['dashboard']['orientation'])) ? $_SESSION['dashboard']['orientation'] : 'L';
 

b3ac97d3dba02adee53086bedac968f733bf96a4
loviuz <loviuz@gmail.com>
2020-02-05 12:49:21 +0100

    Rimozione controlli duplicati su scadenzario

--- modules/scadenzario/actions.php
+++ modules/scadenzario/actions.php
@@ -20,16 +20,6 @@ switch (post('op')) {
         $descrizione = post('descrizione');
         $iddocumento = post('iddocumento') ?: 0;
 
-        // Calcolo il totale da pagare
-        $scadenza = $dbo->fetchOne('SELECT SUM(da_pagare) AS totale_da_pagare, iddocumento FROM co_scadenziario GROUP BY iddocumento HAVING iddocumento=(SELECT iddocumento FROM co_scadenziario s WHERE id='.prepare($id_record).')');
-        $totale_da_pagare = sum($scadenza['totale_da_pagare'], null);
-
-        // Verifico se il totale sommato è uguale al totale da pagare (solo per le scadenze delle fatture)
-        $totale_utente = 0;
-        foreach (post('da_pagare') as $id_scadenza => $da_pagare) {
-            $totale_utente = sum($totale_utente, $da_pagare);
-        }
-
         $totale_pagato = 0;
         $id_scadenza_non_completa = null;
         foreach (post('da_pagare') as $id => $da_pagare) {
@@ -78,19 +68,6 @@ switch (post('op')) {
         }
 
         flash()->info(tr('Scadenze aggiornate!'));
-
-        if ($totale_pagato == $totale_da_pagare) {
-            flash()->warning(tr('Le scadenze sono state completate!'));
-
-            redirect(ROOTDIR.'/controller.php?id_module='.$id_module);
-            Filter::set('post', 'backto', null);
-        } else {
-            $id_record = $id_scadenza_non_completa;
-        }
-
-        if ($totale_da_pagare != $totale_utente) {
-            flash()->error(tr('ATTENZIONE: il totale degli importi inseriti non corrisponde al totale da pagare!'));
-        }
 
         break;
 

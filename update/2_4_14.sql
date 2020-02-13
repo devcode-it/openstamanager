d57cb85b567dfb8fbef493c1db50f8fd03027c79
Luca <lucas@openstamanager.com>
2020-02-13 18:23:53 +0100

    Aumento dimensione campo qta
    Aggiungo come completatato lo stato "Accettato" e "Parzialmente evaso" dell'ordine

--- update/2_4_14.sql
+++ update/2_4_14.sql
@@ -13,3 +13,21 @@ INSERT INTO `zz_settings` (`id`, `nome`, `valore`, `tipo`, `editable`, `sezione`
 
 
 UPDATE `zz_settings` SET `help` = 'Esegue automaticamente un backup completo del gestionale al primo accesso della giornata.' WHERE `zz_settings`.`nome` = 'Backup automatico';
+
+-- Aggiungo come completatato lo stato "Accettato" e "Parzialmente evaso" dell'ordine
+UPDATE `or_statiordine` SET `completato` = '1' WHERE `or_statiordine`.`descrizione` = 'Accettato' OR `or_statiordine`.`descrizione` = 'Parzialmente evaso';
+
+-- Aumento dimensione campo qta
+ALTER TABLE `co_righe_preventivi` CHANGE `qta` `qta` DECIMAL(14,6) NOT NULL;
+ALTER TABLE `co_righe_contratti` CHANGE `qta` `qta` decimal(14, 6) NOT NULL;
+ALTER TABLE `co_righe_documenti` CHANGE `qta` `qta` decimal(14, 6) NOT NULL;
+ALTER TABLE `dt_righe_ddt` CHANGE `qta` `qta` decimal(14, 6) NOT NULL;
+ALTER TABLE `mg_articoli` CHANGE `qta` `qta` decimal(14, 6) NOT NULL;
+ALTER TABLE `mg_movimenti` CHANGE `qta` `qta` decimal(14, 6) NOT NULL;
+ALTER TABLE `or_righe_ordini` CHANGE `qta` `qta` decimal(14, 6) NOT NULL;
+ALTER TABLE `or_righe_ordini` CHANGE `qta_evasa` `qta_evasa` decimal(14, 6) NOT NULL;
+ALTER TABLE `dt_righe_ddt` CHANGE `qta_evasa` `qta_evasa` decimal(14, 6) NOT NULL;
+ALTER TABLE `co_righe_preventivi` CHANGE `qta_evasa` `qta_evasa` decimal(14, 6) NOT NULL;
+ALTER TABLE `co_righe_documenti` CHANGE `qta_evasa` `qta_evasa` decimal(14, 6) NOT NULL;
+ALTER TABLE `co_righe_contratti` CHANGE `qta_evasa` `qta_evasa` decimal(14, 6) NOT NULL;
+ALTER TABLE `mg_articoli` CHANGE `threshold_qta` `threshold_qta` decimal(14, 6) NOT NULL;

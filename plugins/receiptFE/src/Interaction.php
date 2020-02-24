cbbc4069abd3df0fad87808aa128e0226b2672a2
Thomas Zilio <Dasc3er@users.noreply.github.com>
2020-02-24 11:25:18 +0100

    Bugfix eliminazione ricevute

--- plugins/receiptFE/src/Interaction.php
+++ plugins/receiptFE/src/Interaction.php
@@ -16,8 +16,7 @@ class Interaction extends Services
         $list = self::getRemoteList();
 
         // Ricerca fisica
-        $names = array_column($list, 'name');
-        $files = self::getFileList($names);
+        $files = self::getFileList($list);
 
         $list = array_merge($list, $files);
 
@@ -50,9 +49,9 @@ class Interaction extends Services
         return $list ?: [];
     }
 
-    public static function getFileList($names = [])
+    public static function getFileList($list = [])
     {
-        $list = [];
+        $names = array_column($list, 'name');
 
         // Ricerca fisica
         $directory = Ricevuta::getImportDirectory();
@@ -60,13 +59,16 @@ class Interaction extends Services
         $files = glob($directory.'/*.xml*');
         foreach ($files as $id => $file) {
             $name = basename($file);
+            $pos = array_search($name, $names);
 
-            if (!in_array($name, $names)) {
+            if ($pos === false) {
                 $list[] = [
                     'id' => $id,
                     'name' => $name,
                     'file' => true,
                 ];
+            } else {
+                $list[$pos]['id'] = $id;
             }
         }
 

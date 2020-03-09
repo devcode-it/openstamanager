870aec890c99a456021649f9aa4f73a20eb6d1bd
Thomas Zilio <Dasc3er@users.noreply.github.com>
2020-03-09 17:40:05 +0100

    Correzione dimensioni barcode

--- templates/barcode/body.php
+++ templates/barcode/body.php
@@ -10,7 +10,13 @@ echo '<style>
 }
 </style>';
 
+$width_factor = 0.33 * 110;
+$height_factor = 25.93;
+
+$size = $settings['width'] / $width_factor;
+$height = $settings['height'] / $height_factor / $size;
+
 $number = 1; // 32
 for ($i = 0; $i < $number; ++$i) {
-    echo '<barcode code="'.$articolo->barcode.'" type="EAN13" height="1" size="1" class="barcode" />';
+    echo '<barcode code="'.$articolo->barcode.'" type="EAN13" height="'.$height.'" size="'.$size.'" class="barcode" />';
 }

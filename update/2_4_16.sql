b7d96630c3e578986222f7aff9b90d5bb2183670
Luca <lucas@openstamanager.com>
2020-05-15 18:04:43 +0200

    Aggiunta rivalsa inps e relativa iva per il calcolo del totale ivato del documento

--- update/2_4_16.sql
+++ update/2_4_16.sql
@@ -1,2 +1,8 @@
 UPDATE `zz_views` SET `search` = 1 WHERE `zz_views`.`name` = 'Descrizione' AND `id_module` = (SELECT `id` FROM `zz_modules` WHERE `name` = 'Categorie documenti');
 UPDATE `zz_views` SET `search` = 1 WHERE `zz_views`.`name` IN ('Categoria', 'Nome', 'Data') AND `id_module` = (SELECT `id` FROM `zz_modules` WHERE `name` = 'Gestione documentale');
+
+
+-- Aggiunta rivalsa inps e relativa iva per il calcolo del totale ivato del documento
+UPDATE `zz_views` SET `query` = 'righe.totale + `co_documenti`.`rivalsainps` + `co_documenti`.`iva_rivalsainps`' WHERE `id_module` = (SELECT `id` FROM `zz_modules` WHERE `name` = 'Fatture di vendita') AND `name` = 'Totale ivato';
+
+UPDATE `zz_views` SET `query` = 'righe.totale + `co_documenti`.`rivalsainps` + `co_documenti`.`iva_rivalsainps`' WHERE `id_module` = (SELECT `id` FROM `zz_modules` WHERE `name` = 'Fatture di acquisto') AND `name` = 'Totale ivato';

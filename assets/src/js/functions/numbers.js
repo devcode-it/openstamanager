081f80ff1be0a4f91b5d6ab800d7fa6a8817f4b6
Dasc3er <dasc3er@users.noreply.github.com>
2020-09-23 14:26:00 +0200

    Aggiunto CHANGELOG di base per la versione 2.4.18

--- assets/src/js/functions/numbers.js
+++ assets/src/js/functions/numbers.js
@@ -1,3 +1,24 @@
+/*
+ * OpenSTAManager: il software gestionale open source per l'assistenza tecnica e la fatturazione
+ * Copyright (C) DevCode s.n.c.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Funzione per inizializzare i campi di input numerici per la gestione integrata del formato impostato per il gestionale.
+ */
 function initNumbers() {
     let inputs = $('.decimal-number').not('.bound');
 

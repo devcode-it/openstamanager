53494ec4e107415758309858097bd25e42e19a1f
MatteoPistorello <pistorellomatteo8@gmail.com>
2020-10-09 12:03:09 +0200

    Migrazione stampe contabili su mpdf

---
+++ templates/partitario_mastrino/footer.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * OpenSTAManager: il software gestionale open source per l'assistenza tecnica e la fatturazione
+ * Copyright (C) DevCode s.n.c.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+echo '
+<table style="color:#aaa; font-size:10px;">
+<tr>
+    <td align="left" style="width:97mm;">
+        '.tr('Stampato con OpenSTAManager il _DATE_', ['_DATE_' => date('d/m/Y')]).'
+    </td>
+
+    <td class="text-right" style="width:97mm;">
+        '.tr('Pagina _PAGE_ di _TOTAL_', [
+        '_PAGE_' => '{PAGENO}',
+        '_TOTAL_' => '{nb}',
+    ]).'
+    </td>
+</tr>
+</table>';

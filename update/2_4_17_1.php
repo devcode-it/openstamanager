8af5c29a8342615cdb354f6484985b7b2f8a7e6d
Luca <lucas@openstamanager.com>
2020-10-15 15:32:11 +0200

    Retrofix per aggiornamento a 2.4.17.1

--- update/2_4_17_1.php
+++ update/2_4_17_1.php
@@ -1,4 +1,6 @@
 <?php
 
-copyr(base_dir().'/files/my_impianti', base_dir().'/files/impianti');
-delete(base_dir().'/files/my_impianti');
+if (file_exists(base_dir().'/files/my_impianti')){
+    copyr(base_dir().'/files/my_impianti', base_dir().'/files/impianti');
+    delete(base_dir().'/files/my_impianti');
+}

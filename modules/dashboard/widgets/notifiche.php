1c69511569cd11c04dcc5502501e69ed4f756526
Luca <lucas@openstamanager.com>
2021-01-08 12:03:10 +0100

    Aggiunta colorazione rossa per notifiche interne scadute

--- modules/dashboard/widgets/notifiche.php
+++ modules/dashboard/widgets/notifiche.php
@@ -64,8 +64,11 @@ foreach ($moduli as $module_id => $note) {
     </tr>';
 
     foreach ($note as $nota) {
+
+        $class = (strtotime($nota->notification_date) < strtotime(date('Y-m-d')) && !empty($nota->notification_date)) ? 'danger' : '';
+
         echo '
-    <tr>
+    <tr class="'.$class.'">
         <td>'.$nota->id_record.'</td>
 
         <td>

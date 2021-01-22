e990983b167a9aa9228d3e9c272ff7a9463d10ea
MatteoPistorello <pistorellomatteo8@gmail.com>
2021-01-22 15:16:57 +0100

    Fix stampa mastrini

--- templates/partitario_mastrino/bottom.php
+++ templates/partitario_mastrino/bottom.php
@@ -18,17 +18,19 @@
  */
 
 include_once __DIR__.'/../../core.php';
-$dare = 0;
-$avere = 0;
-for ($i = 0; $i < sizeof($records); ++$i) {
-    if ($records[$i]['totale'] >= 0) {
-        $dare += $records[$i]['totale'];
-    } else {
-        $avere += $records[$i]['totale'];
-    }
-}
+
+
 
 if (get('lev') == '2' || get('lev') == '3') {
+    $dare = 0;
+    $avere = 0;
+    for ($i = 0; $i < sizeof($records); ++$i) {
+        if ($records[$i]['totale'] >= 0) {
+            $dare += $records[$i]['totale'];
+        } else {
+            $avere += $records[$i]['totale'];
+        }
+    }
     echo '
     <tr>
         <th></th>
@@ -37,42 +39,49 @@ if (get('lev') == '2' || get('lev') == '3') {
         <th class="text-right">'.moneyFormat(abs($avere)).'</th>
     </tr>';
 } elseif (get('lev') == '1') {
-    $pareggio_dare = abs($dare);
-    $pareggio_avere = abs($avere);
+    $totale_attivo = 0;
+    $totale_passivo = 0;
+    for ($i = 0; $i < sizeof($patrimoniale); ++$i) {
+        if ($patrimoniale[$i]['totale'] >= 0) {
+            $totale_attivo += $patrimoniale[$i]['totale'];
+        } else {
+            $totale_passivo += $patrimoniale[$i]['totale'];
+        }
+    }
     echo '</table>
     <table class="table table-striped table-bordered">
     <tr>
         <th width="25%">TOTALE ATTIVITÀ</th>
-        <th width="25%" class="text-right">'.moneyFormat(abs($dare)).'</th>
+        <th width="25%" class="text-right">'.moneyFormat(abs($totale_attivo)).'</th>
         <th width="25%">PASSIVITÀ</th>
-        <th width="25%" class="text-right">'.moneyFormat(abs($avere)).'</th>
+        <th width="25%" class="text-right">'.moneyFormat(abs($totale_passivo)).'</th>
     </tr>
     <tr>';
 
-    if ($risultato['totale'] <= 0) {
+    if ($utile_perdita['totale'] <= 0) {
         echo '  
             <th></th>
             <th></th>
             <th>UTILE</th>
-            <th class="text-right">'.moneyFormat(abs($risultato['totale'])).'</th>
+            <th class="text-right">'.moneyFormat(abs($utile_perdita['totale'])).'</th>
         </tr>';
-        $pareggio_avere += abs($risultato['totale']);
+        $totale_passivo = abs($totale_passivo + $utile_perdita['totale']);
     } else {
         echo '  
             <th>PERDITA</th>
-            <th class="text-right">'.moneyFormat(abs($risultato['totale'])).'</th>
+            <th class="text-right">'.moneyFormat(abs($utile_perdita['totale'])).'</th>
             <th></th>
             <th></th>
         </tr>';
-        $pareggio_dare += abs($risultato['totale']);
+        $totale_attivo = abs($totale_attivo + $utile_perdita['totale']);
     }
 
     echo '
     <tr>
         <th>TOTALE A PAREGGIO</th>
-        <th class="text-right">'.moneyFormat(abs($pareggio_dare)).'</th>
+        <th class="text-right">'.moneyFormat(abs($totale_attivo)).'</th>
         <th>TOTALE A PAREGGIO</th>
-        <th class="text-right">'.moneyFormat(abs($pareggio_avere)).'</th>
+        <th class="text-right">'.moneyFormat(abs($totale_passivo)).'</th>
     </tr>
     </table>';
 }

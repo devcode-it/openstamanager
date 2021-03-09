bdca50707a49375d1c950ddd1361a83d0af15053
Matteo <matteo.bacca@gmail.com>
2021-03-09 18:36:28 +0100

    Fix pagamento default in creazione documento

--- modules/fatture/actions.php
+++ modules/fatture/actions.php
@@ -690,7 +690,12 @@ switch (post('op')) {
 
             $fattura = Fattura::build($documento->anagrafica, $tipo, post('data'), post('id_segment'));
 
-            $fattura->idpagamento = $documento->idpagamento;
+            if(!empty($documento->idpagamento)){
+                $fattura->idpagamento = $documento->idpagamento;
+            }else{
+                $fattura->idpagamento = setting('Tipo di pagamento predefinito');
+            }
+            
             $fattura->idsede_destinazione = $documento->idsede;
             $fattura->id_ritenuta_contributi = post('id_ritenuta_contributi') ?: null;
 

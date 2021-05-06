e1408aaf13edbd7627f07854cfe196312c97ed16
MatteoPistorello <pistorellomatteo8@gmail.com>
2021-05-06 17:58:40 +0200

    bug data_conclusione in contratti

--- plugins/pianificazione_interventi/edit.php
+++ plugins/pianificazione_interventi/edit.php
@@ -103,11 +103,6 @@ if (!$elenco_promemoria->isEmpty()) {
             $title = 'Elimina promemoria...';
         }
 
-        // data_conclusione contratto
-        if (date('Y', strtotime($contratto['data_conclusione'])) < 1971) {
-            $contratto['data_conclusione'] = '';
-        }
-
         // Informazioni sugli impianti
         $info_impianti = '';
         if (!empty($promemoria['idimpianti'])) {

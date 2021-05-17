e9f4472147c8bd4a062120d8d4e09112f580738b
MatteoPistorello <pistorellomatteo8@gmail.com>
2021-05-17 17:15:49 +0200

    Aggiunto delete allegati quando si elimina una fattura

--- modules/fatture/actions.php
+++ modules/fatture/actions.php
@@ -297,6 +297,12 @@ switch (post('op')) {
 
             // Azzeramento collegamento della rata contrattuale alla pianificazione
             $dbo->query('UPDATE co_fatturazione_contratti SET iddocumento=0 WHERE iddocumento='.prepare($id_record));
+            
+            // Eliminazione allegati collegati
+            Uploads::deleteLinked([
+                'id_module' => $id_module,
+                'id_record' => $id_record,
+            ]);
 
             flash()->info(tr('Fattura eliminata!'));
         } catch (InvalidArgumentException $e) {

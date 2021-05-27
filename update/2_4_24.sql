a8c76ffec2dc2a8c78491ba08f9fadd34298658a
MatteoPistorello <pistorellomatteo8@gmail.com>
2021-05-27 17:56:34 +0200

    Formattazione colonna data modulo Ordini fornitore

--- update/2_4_24.sql
+++ update/2_4_24.sql
@@ -1,3 +1,6 @@
 -- Colonna n. protocollo per fatture di acquisto
 INSERT INTO `zz_views` (`id_module`, `name`, `query`, `order`, `search`, `slow`, `format`, `search_inside`, `order_by`, `visible`, `summable`, `default`) VALUES
 ((SELECT `id` FROM `zz_modules` WHERE name='Fatture di acquisto'), 'N. Prot.', 'co_documenti.numero', 1, 1, 0, 0, '', '', 0, 0, 0);
+ 
+-- Formattazione colonna data modulo Ordini fornitore
+UPDATE `zz_views` SET `format`=1 WHERE `zz_views`.`name`='Data' AND `zz_views`.`id_module`=(SELECT `id` FROM `zz_modules` WHERE name='Ordini fornitore');

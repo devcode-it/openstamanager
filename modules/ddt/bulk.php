4171510c39ab0b7fb42107144966ede716c3aff6
MatteoPistorello <pistorellomatteo8@gmail.com>
2021-06-11 10:24:13 +0200

    Bug creazione fattura da bulk ddt

--- modules/ddt/bulk.php
+++ modules/ddt/bulk.php
@@ -112,6 +112,8 @@ switch (post('op')) {
                             if ($copia->isArticolo()) {
                                 $copia->serials = $riga->serials;
                             }
+
+                            $copia->save();
                         }
                     }
                 }

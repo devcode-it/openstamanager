e95a36991d8b6679bb995ab2d57fa54748eaa8a5
Maicol Battistini <maicolbattistini@live.it>
2021-07-30 18:38:43 +0200

    feat: 🏗️ Introduzione Laravel 8

--- update/create_updates.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `updates` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `directory` varchar(255),
-  `version` varchar(255) NOT NULL,
-  `sql` boolean NOT NULL,
-  `script` boolean NOT NULL,
-  `done` int(11),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB;

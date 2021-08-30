31eef088f5b41eff98062960b60ddf268c64c92b
Dasc3er <Dasc3er@users.noreply.github.com>
2021-08-30 16:27:59 +0200

    Fix autoselezione ricerca in apertura select

--- assets/src/js/base/custom.js
+++ assets/src/js/base/custom.js
@@ -97,3 +97,14 @@ $(document).ready(function () {
         });
     }, 1000);
 });
+
+/*
+ * Hacky fix for a bug in select2 with jQuery 3.6.0's new nested-focus "protection"
+ * see: https://github.com/select2/select2/issues/5993
+ * see: https://github.com/jquery/jquery/issues/4382
+ *
+ * TODO: Recheck with the select2 GH issue and remove once this is fixed on their side
+ */
+$(document).on('select2:open', () => {
+    document.querySelector('.select2-search__field').focus();
+});

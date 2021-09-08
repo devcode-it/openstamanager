4bafd23a55eb9a75e2682ce3679cbce118cf85d7
Maicol Battistini <maicolbattistini@live.it>
2021-09-08 16:17:26 +0200

    chore(deps): ➕ Aggiunto pacchetto per configurazione Vite
    La configurazione di Vite ora è importata da un pacchetto creato ad-hoc

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { esbuildFlowPlugin, flowPlugin } from '@bunchtogether/vite-plugin-flow';
+import osmConfig from '@openstamanager/vite-config';
 import { defineConfig } from 'laravel-vite';
 // import ViteFonts from 'vite-plugin-fonts';
 
-export default defineConfig({
-  assetsInclude: ['js', 'png'],
+export default defineConfig(osmConfig({
   /* NOT WORKING: waiting a fix
   css: {
     preprocessorOptions: {
@@ -12,46 +11,10 @@ export default defineConfig({
       },
     },
   }, */
-  build: {
-    minify: false,
-    rollupOptions: {
-      manualChunks: {},
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      },
-      preserveEntrySignatures: 'allow-extension'
-    }
-  },
-  esbuild: {
-    jsxFactory: 'm',
-    jsxFragment: 'm.Fragment'
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [esbuildFlowPlugin(/\.(flow|jsx?)$/, path => (/\.jsx$/.test(path) ? 'jsx' : 'js'), {
-        all: true,
-        pretty: true,
-        ignoreUninitializedFields: false
-      })]
-    }
-  },
-  plugins: [
-    flowPlugin({
-      include: /\.(flow|jsx?)$/,
-      exclude: /node_modules/,
-      flow: {
-        all: true,
-        pretty: true,
-        ignoreUninitializedFields: false
-      }
-    })
-    /* NOT WORKING. Waiting a fix
+  /* NOT WORKING. Waiting a fix
     ViteFonts({
       google: {
         families: ['Montserrat', 'Nunito']
       }
     }) */
-  ]
-});
+}));

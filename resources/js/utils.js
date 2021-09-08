c38c6a88ea358b202c38c0a3898db4ee289bdb15
Maicol Battistini <maicolbattistini@live.it>
2021-09-08 16:14:22 +0200

    refactor: ♻️ Scrittura migliore per tipi funzione

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -1,7 +1,7 @@
 /**
  * Check if class/object A is the same as or a subclass of class B.
  */
-export default function subclassOf(A: object, B: object) {
+export default function subclassOf(A: Object, B: Object) {
   // noinspection JSUnresolvedVariable
   return A && (A === B || A.prototype instanceof B);
 }

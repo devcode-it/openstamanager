0a03e9c1aa177e6d98309b148324824bc0b3f2e9
Maicol Battistini <maicolbattistini@live.it>
2021-09-11 11:01:08 +0200

    feat(dev): ✨ Aggiunto livereload

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import osmConfig from '@openstamanager/vite-config';
-import { defineConfig } from 'laravel-vite';
-// import ViteFonts from 'vite-plugin-fonts';
+import {defineConfig} from 'laravel-vite';
+import livereload from 'rollup-plugin-livereload';
 
 export default defineConfig(osmConfig({
-  /* NOT WORKING: waiting a fix
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '$mdi-font-path: "./build";',
-      },
-    },
-  }, */
-  /* NOT WORKING. Waiting a fix
-    ViteFonts({
-      google: {
-        families: ['Montserrat', 'Nunito']
-      }
-    }) */
+  plugins: [
+    livereload('public/build')
+  ]
 }));

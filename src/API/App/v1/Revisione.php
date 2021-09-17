5dceaa5528ac642ccc5b8da02f7f7c09aac601d8
Dasc3er <Dasc3er@users.noreply.github.com>
2021-09-17 13:34:57 +0200

    Modifica numero revisione API App

--- src/API/App/v1/Revisione.php
+++ src/API/App/v1/Revisione.php
@@ -24,7 +24,7 @@ use API\Resource;
 
 class Revisione extends Resource implements RetrieveInterface
 {
-    const REVISION = '1.3';
+    const REVISION = '5';
 
     public function retrieve($request)
     {

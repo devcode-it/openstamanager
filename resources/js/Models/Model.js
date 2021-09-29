2b5ff02b2874cba8ea26a015899be714b6b60eec
Maicol Battistini <maicolbattistini@live.it>
2021-09-29 20:29:08 +0200

    feat: ✨ Permetti path custom

--- resources/js/Models/Model.js
+++ resources/js/Models/Model.js
@@ -1,9 +1,16 @@
 import BaseModel from 'javel';
+import {snakeCase} from 'lodash';
 import redaxios from 'redaxios';
 
 export default class Model extends BaseModel {
+  urlPath: string;
+
   baseUrl() {
     return '/api';
+  }
+
+  buildUrl({params}): Array {
+    return [this.urlPath ?? snakeCase(this.constructor.name), ...params];
   }
 
   makeRequest({

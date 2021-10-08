e1e0369f4b6cdc3f753f240ef127f3e5e2e08172
MatteoPistorello <pistorellomatteo8@gmail.com>
2021-10-08 10:00:36 +0200

    Fix cambio stato intervento in fase di eliminazione riga da fattura

--- modules/fatture/src/Components/RelationTrait.php
+++ modules/fatture/src/Components/RelationTrait.php
@@ -182,7 +182,7 @@ trait RelationTrait
     {
         $result = parent::delete();
 
-        if (!empty($this->idintervento)) {
+        if (!empty($this->idintervento) && $this->fattura->getRighe()->where('idintervento', $this->idintervento)->count()==1) {
             database()->query("UPDATE in_interventi SET idstatointervento = (SELECT idstatointervento FROM in_statiintervento WHERE codice = 'OK') WHERE id=".prepare($this->idintervento));
         }
 

4e270c99947d77e713694eb2c0fbc5841e97d68b
Maicol Battistini <maicolbattistini@live.it>
2021-11-23 21:24:29 +0100

    feat: ✨ Cambia stato alla checkbox dell'header quando cambi stato a una checkbox di una riga

--- resources/js/Components/DataTable/TableCell.jsx
+++ resources/js/Components/DataTable/TableCell.jsx
@@ -1,3 +1,6 @@
+import {type Cash} from 'cash-dom/dist/cash';
+import {inRange} from 'lodash-es';
+
 import Component from '../Component.jsx';
 
 /**
@@ -15,5 +18,31 @@ export default class TableCell extends Component {
     }
 
     return <td {...this.attrs.all()}>{vnode.children}</td>;
+  }
+
+  oncreate(vnode) {
+    super.oncreate(vnode);
+
+    const checkboxes = (): Cash => $(this.element)
+      .closest('.mdc-data-table')
+      .find('tbody tr[checkable] mwc-checkbox');
+
+    const cell: Cash = $(this.element);
+    cell.children('mwc-checkbox').on('change', (event) => {
+      const row = cell.parent();
+      row.toggleClass('mdc-data-table__row--selected');
+
+      const headerCheckbox = cell.closest('.mdc-data-table').find('thead th mwc-checkbox');
+      const checks = checkboxes();
+      const checked = checks.filter('[checked]');
+
+      if (inRange(checked.length, 1, checks.length)) {
+        headerCheckbox.prop('indeterminate', true);
+        headerCheckbox.prop('checked', false);
+      } else {
+        headerCheckbox.prop('checked', checks.length === checked.length);
+        headerCheckbox.prop('indeterminate', false);
+      }
+    });
   }
 }

d2f0334e22b44646ee9bab4b9ae119be60583448
MatteoPistorello <pistorellomatteo8@gmail.com>
2021-12-23 10:12:22 +0100

    Fix options modulo Stampe

--- update/2_4_29.sql
+++ update/2_4_29.sql
@@ -3,3 +3,6 @@ ALTER TABLE `co_contratti` ADD `condizioni_fornitura` TEXT NOT NULL AFTER `infor
 
 UPDATE `zz_settings` SET `nome` = 'Condizioni generali di fornitura preventivi' WHERE `zz_settings`.`nome` = 'Condizioni generali di fornitura';
 INSERT INTO `zz_settings` (`id`, `nome`, `valore`, `tipo`, `editable`, `sezione`, `order`, `help`) VALUES (NULL, 'Condizioni generali di fornitura contratti', '', 'ckeditor', '1', 'Contratti', NULL, NULL);
+
+-- Rimossa visualizzazione stampe disabilitate
+UPDATE `zz_modules` SET `options` = 'SELECT |select| FROM `zz_prints` WHERE 1=1 AND enabled=1 HAVING 2=2' WHERE `zz_modules`.`name` = 'Stampe'; 

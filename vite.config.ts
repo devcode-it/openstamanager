c9fb53ec45046638d41268e42839a2cb9e4cd384
Maicol Battistini <maicolbattistini@live.it>
2022-01-06 18:14:13 +0100

    fix(tsconfig): 🔧 Fix `includePaths`
    - Config file di Vite mancante
    - Includi solo `resources/js/*` e non anche le altre cartelle dentro `resources`

--- vite.config.ts
+++ vite.config.ts
@@ -24,18 +24,18 @@ export default defineConfig(osmConfig({
           {
             src: 'android-chrome-192x192.png',
             sizes: '192x192',
-            type: 'image/png',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
           },
           {
             src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-          },
-          {
-            src: 'android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
+            purpose: 'any maskable'
           }
         ]
       },

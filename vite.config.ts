f28cfcb6fb34cb54812617a8affca6075c003c08
Maicol Battistini <maicolbattistini@live.it>
2022-01-07 14:38:18 +0100

    build: 💚 Fix Vite PWA (antfu/vite-plugin-pwa#199)

--- vite.config.ts
+++ vite.config.ts
@@ -3,43 +3,40 @@ import osmConfig from '@openstamanager/vite-config';
 import {defineConfig} from 'laravel-vite';
 import {VitePWA} from 'vite-plugin-pwa';
 
-export default defineConfig(osmConfig({
-  plugins: [
-    VitePWA({
-      includeAssets: [
-        'images/favicon/favicon.ico',
-        '../robots.txt',
-        'images/favicon/apple-touch-icon.png',
-        'images/*.png'
-      ],
-      manifest: {
-        name: 'OpenSTAManager',
-        short_name: 'OSM',
-        description: 'Il software gestionale open source per l\'assistenza tecnica e la fatturazione',
-        categories: ['business', 'productivity'],
-        display: 'minimal-ui',
-        theme_color: '#3f3f3f',
-        background_color: '#fffff',
-        icons: [
-          {
-            src: 'android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      },
-      useCredentials: true
-    })
-  ]
-}));
+export default defineConfig(osmConfig())
+  .withPlugins(...VitePWA({
+    includeAssets: [
+      'images/favicon/favicon.ico',
+      '../robots.txt',
+      'images/favicon/apple-touch-icon.png',
+      'images/*.png'
+    ],
+    manifest: {
+      name: 'OpenSTAManager',
+      short_name: 'OSM',
+      description: 'Il software gestionale open source per l\'assistenza tecnica e la fatturazione',
+      categories: ['business', 'productivity'],
+      display: 'minimal-ui',
+      theme_color: '#3f3f3f',
+      background_color: '#fffff',
+      icons: [
+        {
+          src: 'android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable'
+        }
+      ]
+    },
+    useCredentials: true
+  }));

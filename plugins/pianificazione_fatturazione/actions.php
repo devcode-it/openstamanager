f797ba12eb3cf3ec198043493f078e217e42bf1e
Beppe <cjbeppe87@gmail.com>
2022-01-14 14:30:25 +0100

    Correzione inclusioni

--- plugins/pianificazione_fatturazione/actions.php
+++ plugins/pianificazione_fatturazione/actions.php
@@ -17,7 +17,6 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
- use Carbon\Carbon;
 use Modules\Articoli\Articolo as ArticoloOriginale;
 use Modules\Contratti\Components\Articolo;
 use Modules\Contratti\Components\Riga;

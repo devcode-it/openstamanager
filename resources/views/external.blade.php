8fc6e4d9b124eef53020b5bc406acbefca2f028e
Maicol Battistini <maicolbattistini@live.it>
2022-01-26 12:03:53 +0100

    fix: 🐛 Variabile non definita

--- resources/views/external.blade.php
+++ resources/views/external.blade.php
@@ -2,6 +2,10 @@
 <html lang="{{app()->getLocale()}}">
     @include('layouts.head')
     <body>
+        @php
+            $modules = app(\App\Http\Controllers\Controller::class)
+                        ->getModules(request());
+        @endphp
         @inertia
         @include('layouts.footer')
     </body>

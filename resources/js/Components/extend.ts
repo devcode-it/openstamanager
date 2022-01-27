860046bef44336af9c273f2075979403e5d3dda0
Maicol Battistini <maicolbattistini@live.it>
2022-01-27 13:37:08 +0100

    feat(extend): ✨ Aggiunte alcune funzioni utili per i moduli
    Permette di eseguire alcune operazioni sulle RecordsPage

--- resources/js/Components/extend.ts
+++ resources/js/Components/extend.ts
@@ -2,9 +2,19 @@
  * @source https://github.com/flarum/core/blob/master/js/src/common/extend.js
  */
 
+import {
+  SelectT,
+  TextAreaT,
+  TextFieldT
+} from '../typings';
+import {
+  ColumnT,
+  RecordsPage
+} from './Pages';
+
 /**
- * Type that returns an array of all keys of a provided object that are of the provided type,
- * or a subtype of the type.
+ * Type that returns an array of all keys of a provided object that are of
+ * of the provided type, or a subtype of the type.
  */
 declare type KeysOfType<Type extends object, Match> = {
   [Key in keyof Type]-?: Type[Key] extends Match ? Key : never;
@@ -107,4 +117,75 @@ export function override<T extends Record<any, any>, K extends KeyOfType<T, Func
 
     Object.assign(object[method], original);
   }
+}
+
+// Additional extenders for RecordsPage
+
+/**
+ * Adds or updates the columns of a RecordsPage
+ *
+ * @param page The page of the columns to add or update
+ * @param columns An object containing the columns to add or update ({id: 'Heading' | {…}})
+ */
+export function updateColumns(
+  page: RecordsPage & {prototype: RecordsPage},
+  columns: Record<string, string | ColumnT>
+) {
+  extend(page.prototype, 'oninit', function (this: RecordsPage) {
+    for (const [id, value] of Object.entries(columns)) {
+      this.columns[id] = value;
+    }
+  });
+}
+
+/**
+ * Deletes the columns of a RecordsPage
+ *
+ * @param page The page of the columns to delete
+ * @param ids The IDs of the columns to delete
+ */
+export function deleteColumns(page: RecordsPage & {prototype: RecordsPage}, ids: string[]) {
+  extend(page.prototype, 'oninit', function (this: RecordsPage) {
+    for (const id of ids) {
+      delete this.columns[id];
+    }
+  });
+}
+
+/**
+ * Adds or updates the fields of a RecordsPage record dialog
+ *
+ * @param page The page of the fields to add or update
+ * @param section The dialog section of the fields to add or update
+ * @param fields
+ */
+export function updateFields(
+  page: RecordsPage & {prototype: RecordsPage},
+  section: string,
+  fields: Record<string, TextFieldT | TextAreaT | SelectT>
+) {
+  extend(page.prototype, 'oninit', function (this: RecordsPage) {
+    for (const [id, value] of Object.entries(fields)) {
+      this.sections[section].fields[id] = value;
+    }
+  });
+}
+
+/**
+ * Delets the fields of a RecordsPage record dialog
+ *
+ * @param page The page of the fields to delete
+ * @param section The dialog section to delete
+ * @param fields The IDs of the fields to delete
+ */
+export function deleteFields(
+  page: RecordsPage & {prototype: RecordsPage},
+  section: string,
+  fields: string[]
+) {
+  extend(page.prototype, 'oninit', function (this: RecordsPage) {
+    for (const id of fields) {
+      delete this.sections[section].fields[id];
+    }
+  });
 }

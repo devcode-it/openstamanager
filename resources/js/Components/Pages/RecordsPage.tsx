becebb8085ebc11f0954edf37c8bc6b263d16f25
Maicol Battistini <maicolbattistini@live.it>
2022-01-27 23:46:13 +0100

    fix: 🏷️ Allow array as return type

--- resources/js/Components/Pages/RecordsPage.tsx
+++ resources/js/Components/Pages/RecordsPage.tsx
@@ -199,7 +199,7 @@ export class RecordsPage extends Page {
     }
   }
 
-  recordDialog() {
+  recordDialog(): JSX.Element | JSX.Element[] {
     return (
       <mwc-dialog
         id="add-record-dialog"

984b028cc5936be17d137f413b9cc8a5bf9f1e0e
Maicol Battistini <maicolbattistini@live.it>
2022-01-27 15:17:50 +0100

    fix: 🐛 Exports mancanti per la cartella `extend`

--- resources/js/Components/index.ts
+++ resources/js/Components/index.ts
@@ -3,7 +3,7 @@
 
 export {default as Component} from './Component';
 export * from './DataTable';
-export {extend, override} from './extend';
+export * from './extend';
 export {default as LoadingButton} from './LoadingButton';
 export {default as Mdi} from './Mdi';
 export {default as Page} from './Page';

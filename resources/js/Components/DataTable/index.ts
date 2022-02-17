dc1bcf6911ceae16e78745d13b6848bca9c82a62
Maicol Battistini <maicolbattistini@live.it>
2022-02-17 11:29:56 +0100

    fix: Export mancante in DataTable

--- resources/js/Components/DataTable/index.ts
+++ resources/js/Components/DataTable/index.ts
@@ -4,4 +4,4 @@ export {default as DataTable} from './DataTable';
 export {default as TableCell} from './TableCell';
 export {default as TableColumn} from './TableColumn';
 export {default as TableFooter} from './TableFooter';
-export * from './TableRow';
+export {default as TableRow} from './TableRow';

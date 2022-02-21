4d282437a07912c14be2c8b55b19c8299be2ef21
Luca <lucas@openstamanager.com>
2022-02-21 10:30:19 +0100

    Aggiunta informazione Esigibilità IVA in fase di creazione
    Fix minori

--- modules/iva/add.php
+++ modules/iva/add.php
@@ -19,42 +19,61 @@
 
 include_once __DIR__.'/../../core.php';
 
+$esigibilita = [
+    [
+        'id' => 'I',
+        'text' => tr('IVA ad esigibilità immediata'),
+    ],
+    [
+        'id' => 'D',
+        'text' => tr('IVA ad esigibilità differita'),
+    ],
+    [
+        'id' => 'S',
+        'text' => tr('Scissione dei pagamenti'),
+    ],
+];
+
 use Modules\Iva\Aliquota;
 
-$codice = Aliquota::max('id')+1;
+$codice = Aliquota::max('codice')+1;
 
 ?><form action="" method="post" id="add-form">
 	<input type="hidden" name="op" value="add">
 	<input type="hidden" name="backto" value="record-edit">
 
 	<div class="row">
+		<div class="col-md-4">
+			{[ "type": "text", "label": "<?php echo tr('Codice'); ?>", "name": "codice", "required": 1, "decimals": 0, "min-value": "0", "max-value": "999", "maxlength": 3, "value": "<?php echo $codice; ?>", "validation": "codice" ]}
+		</div>
+
 		<div class="col-md-8">
 			{[ "type": "text", "label": "<?php echo tr('Descrizione'); ?>", "name": "descrizione", "required": 1 ]}
 		</div>
-
-		<div class="col-md-4">
-			{[ "type": "number", "label": "<?php echo tr('Codice'); ?>", "name": "codice", "required": 1, "decimals":0, "min-value":"0", "max-value":"999", "maxlength": 3, "value": "<?php echo $codice; ?>", "validation": "codice" ]}
-		</div>
 	</div>
 
 	<div class="row">
-        <div class="col-md-6">
+        <div class="col-md-4">
             {[ "type": "checkbox", "label": "<?php echo tr('Esente'); ?>", "name": "esente", "id": "esente-add", "value": "$esente$" ]}
         </div>
 
-		<div class="col-md-6">
+		<div class="col-md-4">
 			{[ "type": "number", "label": "<?php echo tr('Percentuale'); ?>", "name": "percentuale", "id": "percentuale-add", "icon-after": "<i class=\"fa fa-percent\"></i>", "max-value": "100" ]}
 		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-6">
-			{[ "type": "number", "label": "<?php echo tr('Indetraibile'); ?>", "name": "indetraibile", "icon-after": "<i class=\"fa fa-percent\"></i>", "max-value": "100" ]}
-		</div>
 
-		<div class="col-md-6">
+		<div class="col-md-4">
 			{[ "type": "select", "label": "<?php echo tr('Codice Natura (Fatturazione Elettronica)'); ?>", "name": "codice_natura_fe", "values": "query=SELECT codice as id, CONCAT(codice, ' - ', descrizione) AS descrizione FROM fe_natura", "extra": "disabled" ]}
 		</div>
 
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+			{[ "type": "number", "label": "<?php echo tr('Indetraibile'); ?>", "name": "indetraibile", "icon-after": "<i class=\"fa fa-percent\"></i>", "max-value": "100" ]}
+		</div>
+
+		<div class="col-md-4">
+			{[ "type": "select", "label": "<?php echo tr('Esigibilità (Fatturazione Elettronica)'); ?>", "name": "esigibilita", "values": <?php echo json_encode($esigibilita); ?>, "required": 1 ]}
+		</div>
 	</div>
 
 	<!-- PULSANTI -->

19a83dff824d25cf4229d4fdf00a53fb8e232e40
Maicol Battistini <maicolbattistini@live.it>
2022-03-10 15:39:26 +0100

    feat: ✨ Aggiunti Barrels TS mancanti

--- resources/js/Components/index.ts
+++ resources/js/Components/index.ts
@@ -4,6 +4,7 @@
 export {default as Component} from './Component';
 export * from './DataTable';
 export * from './extend';
+export * from './layout';
 export {default as LoadingButton} from './LoadingButton';
 export {default as Mdi} from './Mdi';
 export {default as Page} from './Page';
---
+++ resources/js/Components/layout/index.ts
@@ -0,0 +1,5 @@
+export * from './Drawer';
+export * from './Drawer';
+export * from './Footer';
+export * from './TopAppBar';
+export * from './TopAppBar';

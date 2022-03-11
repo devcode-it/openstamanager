343730912d836f42a78ee0ee60d5e3f9cf75869a
Maicol Battistini <maicolbattistini@live.it>
2022-03-11 12:36:00 +0100

    fix: 🐛 Fix attributi pagina e props

--- resources/js/Components/Page.tsx
+++ resources/js/Components/Page.tsx
@@ -7,36 +7,38 @@ import {
 import Component from './Component';
 import TopAppBar from './layout/TopAppBar';
 
-type PageAttributes = {
-  component: string
-  locale: string
-  props: Record<string, any> & {external?: boolean}
-  url: string
-  version: string
-};
+export interface PageAttributes {
+  page: {
+    component: string
+    locale: string
+    props: Record<string, any> & {external?: boolean}
+    url: string
+    version: string
+  };
+}
 
 /**
  * The `Page` component
  *
  * @abstract
  */
-export default abstract class Page extends Component<{}> {
-  page: PageAttributes = JSON.parse($('#app')
-    .attr('data-page') as string) as PageAttributes;
+export default abstract class Page extends Component<PageAttributes> {
   title?: string;
 
-  view(vnode: Vnode) {
+  view(vnode: Vnode<PageAttributes>) {
     let contents = this.contents();
     if (contents instanceof Collection) {
       contents = contents.toArray();
     }
 
-    return this.page.props.external ? contents : (
+    return vnode.attrs.page.props.external ? contents : (
       <TopAppBar>
         {contents}
       </TopAppBar>
     );
   }
 
-  abstract contents(): Children | Collection<Children>;
+  contents(): Children | Collection<Children> {
+    return undefined;
+  }
 }

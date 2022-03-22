a28ffe5b578ecc64b651a9a7cfb225a8fb2158bc
MatteoPistorello <pistorellomatteo8@gmail.com>
2022-03-22 14:57:38 +0100

    Fix selectSetNew da creazione al volo

--- add.php
+++ add.php
@@ -86,9 +86,9 @@ $(document).ready(function(){
             id_plugin: "'.$id_plugin.'",
         }).then(function(response) {
             // Selezione automatica nuovo valore per il select
-            var select = input("'.get('select').'");
+            var select = $("#'.get('select').'");
             if (select) {
-                select.getElement().selectSetNew(response.id, response.text, response.data);
+                select.selectSetNew(response.id, response.text, response.data);
                 //select.change();
             }
 

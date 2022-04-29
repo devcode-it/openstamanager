13d775f90b281a501f74eff8dace1ff2cbc12e5a
MatteoPistorello <pistorellomatteo8@gmail.com>
2022-04-29 09:22:26 +0200

    Fix minore variables Contratti

--- modules/contratti/variables.php
+++ modules/contratti/variables.php
@@ -20,6 +20,7 @@
 $r = $dbo->fetchOne('SELECT *,
     an_anagrafiche.pec,
     IF((an_referenti.email IS NOT NULL AND an_referenti.email != ""), an_referenti.email, an_anagrafiche.email) AS email,
+    an_anagrafiche.idanagrafica,
     an_anagrafiche.ragione_sociale
 FROM co_contratti
     INNER JOIN an_anagrafiche ON co_contratti.idanagrafica=an_anagrafiche.idanagrafica

080126b59f74b5cf917da93872f3cbbdaeb1581e
MatteoPistorello <pistorellomatteo8@gmail.com>
2022-05-16 17:17:05 +0200

    Fix css personalizzato

--- index.php
+++ index.php
@@ -188,7 +188,7 @@ echo ' required>
             });
             </script>';
 
-$custom_css = html_entity_decode(setting('CSS Personalizzato'));
+$custom_css = $dbo->isInstalled() ? html_entity_decode(setting('CSS Personalizzato')) : '';
 if (!empty($custom_css)) {
     echo '
     <style>'.$custom_css.'</style>';

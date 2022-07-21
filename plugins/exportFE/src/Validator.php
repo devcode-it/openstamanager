8acfc21387dbab17077d8223f90cb1e791864bc5
Luca <lucas@openstamanager.com>
2022-07-21 15:30:19 +0200

    Sostituzione dei caratteri speciali in fase di generazione FE

--- plugins/exportFE/src/Validator.php
+++ plugins/exportFE/src/Validator.php
@@ -631,14 +631,7 @@ class Validator
 
             // Formattazione testo
             elseif ($info['type'] == 'string' || $info['type'] == 'normalizedString') {
-                /**
-                 * Sostituzione dei caratteri speciali del linguaggio.
-                 *
-                 * @source https://stackoverflow.com/a/3373364
-                 */
-                /*$replace_chars = ['Š' => 'S', 'š' => 's', 'Ž' => 'Z', 'ž' => 'z', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c', 'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ý' => 'y', 'þ' => 'b', 'ÿ' => 'y'];
-                $output = replace($output, $replace_chars);*/
-
+                
                 // Sostituzione tag e contenuti specifici per XML
                 $output = htmlspecialchars($output, ENT_NOQUOTES | ENT_XML1 | ENT_SUBSTITUTE, 'UTF-8', false);
 
@@ -649,6 +642,10 @@ class Validator
                     '…' => '...',
                     '~' => '-',
                     '®' => '',
+                    'Š' => 'S',
+                    'š' => 's',
+                    'Ž' => 'Z',
+                    'ž' => 'z',
                 ]);
 
                 //$output = self::sanitizeXML($output);

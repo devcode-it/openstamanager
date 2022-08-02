1bb6940d1f77bd72769a878ed1706ecdc7f8d061
loviuz <loviuz@gmail.com>
2022-08-02 17:11:21 +0200

    Fix nome plugin

--- include/common/importa.php
+++ include/common/importa.php
@@ -17,7 +17,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-use Plugins\DettagliArticolo\DettaglioFornitore;
+use Plugins\ListinoFornitori\DettaglioFornitore;
 
 // Inizializzazione
 $documento = $options['documento'];

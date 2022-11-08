a1f1ba2eedc6bb82bb2ab08c0e4d72dfdf617167
Pek5892 <pek5892@gmail.com>
2022-11-08 16:46:59 +0100

    fix minore

--- update/2_4_38.sql
+++
@@ -1,8 +0,0 @@
--- 
-ALTER TABLE `an_anagrafiche` CHANGE `colore` `colore` VARCHAR(30) NOT NULL DEFAULT '#FFFFFF'; 
-ALTER TABLE `mg_categorie` CHANGE `colore` `colore` VARCHAR(30) NOT NULL; 
-ALTER TABLE `my_impianti_categorie` CHANGE `colore` `colore` VARCHAR(30) NOT NULL; 
-ALTER TABLE `an_provenienze` CHANGE `colore` `colore` VARCHAR(30) NOT NULL;
-ALTER TABLE `an_relazioni` CHANGE `colore` `colore` VARCHAR(30) NOT NULL; 
-ALTER TABLE `in_statiintervento` CHANGE `colore` `colore` VARCHAR(30) NOT NULL DEFAULT '#FFFFFF'; 
-ALTER TABLE `co_statipreventivi` CHANGE `colore` `colore` VARCHAR(30) NOT NULL; 

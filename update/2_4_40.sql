382173c2b2d464dcccd35f12cc2c97180713af06
Pek5892 <pek5892@gmail.com>
2023-01-16 12:49:35 +0100

    Fix query vista utenti e permessi

---
+++ update/2_4_40.sql
@@ -0,0 +1,13 @@
+-- Fix query viste Utenti e permessi
+UPDATE `zz_modules` SET `options` = "SELECT
+    |select|
+FROM 
+    `zz_groups` 
+    LEFT JOIN (SELECT `zz_users`.`idgruppo`, COUNT(`id`) AS num FROM `zz_users` GROUP BY `idgruppo`) AS utenti ON `zz_groups`.`id`=`utenti`.`idgruppo`
+WHERE 
+    1=1
+HAVING 
+    2=2 
+ORDER BY 
+    `id`, 
+    `nome` ASC" WHERE `name` = 'Utenti e permessi';

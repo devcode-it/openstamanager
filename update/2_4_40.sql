b565a5d6b8a0bbdfcc20388b7f14c80c084c0c55
Pek5892 <pek5892@gmail.com>
2023-01-17 13:16:36 +0100

    Aggiunta descrizione codice natura N7

--- update/2_4_40.sql
+++ update/2_4_40.sql
@@ -50,3 +50,6 @@ HAVING
     2=2
 ORDER BY
     TRIM(`ragione_sociale`)" WHERE `name` = 'Anagrafiche';
+
+-- Aggiunta descrizione codice natura N7
+INSERT INTO `co_iva` (`id`, `descrizione`, `percentuale`, `indetraibile`, `esente`, `dicitura`, `codice_natura_fe`, `deleted_at`, `codice`, `esigibilita`, `default`) VALUES (NULL, 'Regime OSS e D.Lgs.\r\n83/2021', '0.00', '0.00', '1', NULL, 'N7', NULL, NULL, 'I', '1'); 

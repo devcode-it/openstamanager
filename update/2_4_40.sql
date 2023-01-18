fa590871f5da117c756f855aecd718f2f2070c6d
Pek5892 <pek5892@gmail.com>
2023-01-18 10:21:16 +0100

    Fix #1124

--- update/2_4_40.sql
+++ update/2_4_40.sql
@@ -43,3 +43,31 @@ ORDER BY
 -- Aggiunta descrizione codice natura N7
 SELECT @codice := MAX(CAST(codice AS UNSIGNED))+1 FROM co_iva WHERE deleted_at IS NULL;
 INSERT INTO `co_iva` (`id`, `descrizione`, `percentuale`, `indetraibile`, `esente`, `dicitura`, `codice_natura_fe`, `deleted_at`, `codice`, `esigibilita`, `default`) VALUES (NULL, 'Regime OSS, D.Lgs. 83/2021', '0.00', '0.00', '1', NULL, 'N7', NULL, @codice, 'I', '1'); 
+
+-- Aggiunta campo agente in Preventivi
+INSERT INTO `zz_views` (`id_module`, `name`, `query`, `order`, `search`, `slow`, `format`, `html_format`, `search_inside`, `order_by`, `visible`, `summable`, `default`) VALUES
+('13', 'Agente', '`agente`.`nome`', '11', '1', '0', '0', '0', NULL, NULL, '0', '0', '0');
+
+UPDATE `zz_modules` SET `options` = "SELECT 
+|select|
+FROM 
+    `co_preventivi`
+    LEFT JOIN `an_anagrafiche` ON `co_preventivi`.`idanagrafica` = `an_anagrafiche`.`idanagrafica`
+    LEFT JOIN `co_statipreventivi` ON `co_preventivi`.`idstato` = `co_statipreventivi`.`id`
+    LEFT JOIN (
+        SELECT `idpreventivo`,
+            SUM(`subtotale` - `sconto`) AS `totale_imponibile`,
+            SUM(`subtotale` - `sconto` + `iva`) AS `totale`
+        FROM `co_righe_preventivi`
+        GROUP BY `idpreventivo`
+    ) AS righe ON `co_preventivi`.`id` = `righe`.`idpreventivo`
+    LEFT JOIN (SELECT `an_anagrafiche`.`idanagrafica`, `an_anagrafiche`.`ragione_sociale` AS nome FROM `an_anagrafiche`)AS agente ON `agente`.`idanagrafica`=`co_preventivi`.`idagente`
+    LEFT JOIN (SELECT GROUP_CONCAT(DISTINCT co_documenti.numero_esterno SEPARATOR ', ') AS info, co_righe_documenti.original_document_id AS idpreventivo FROM co_documenti INNER JOIN co_righe_documenti ON co_documenti.id = co_righe_documenti.iddocumento WHERE original_document_type='Modules\\Preventivi\\Preventivo' GROUP BY idpreventivo) AS fattura ON fattura.idpreventivo = co_preventivi.id
+WHERE 
+    1=1 |segment(`co_preventivi`.`id_segment`)| |date_period(custom,'|period_start|' >= `data_bozza` AND '|period_start|' <= `data_conclusione`,'|period_end|' >= `data_bozza` AND '|period_end|' <= `data_conclusione`,`data_bozza` >= '|period_start|' AND `data_bozza` <= '|period_end|',`data_conclusione` >= '|period_start|' AND `data_conclusione` <= '|period_end|',`data_bozza` >= '|period_start|' AND `data_conclusione` = '0000-00-00')| AND default_revision = 1
+GROUP BY 
+    `co_preventivi`.`id`
+HAVING 
+    2=2
+ORDER BY 
+    `co_preventivi`.`id` DESC" WHERE `name` = 'Preventivi';

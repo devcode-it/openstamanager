f91edb9f946dde459ccf177f04c1094c31a28b2c
Pek5892 <pek5892@gmail.com>
2023-04-17 17:36:16 +0200

    Fix #1253

--- modules/ddt/actions.php
+++ modules/ddt/actions.php
@@ -656,6 +656,8 @@ switch (filter('op')) {
                 }
                 if ($dir == 'entrata') {
                     $prezzo_unitario = $prezzo_unitario ?: ($prezzi_ivati ? $originale->prezzo_vendita_ivato : $originale->prezzo_vendita);
+                } else {
+                    $prezzo_unitario = $prezzo_unitario ?: $originale->prezzo_acquisto;
                 }
                 
 

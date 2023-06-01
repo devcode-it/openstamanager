201f59bd556788bb4d8e5d4604062ed7ab6f68e5
Maicol Battistini <maicolbattistini@live.it>
2023-06-01 17:28:13 +0200

    chore: Usa solo `href` nel `DrawerEntry`

--- resources/ts/Components/layout/DrawerEntry.tsx
+++ resources/ts/Components/layout/DrawerEntry.tsx
@@ -14,22 +14,21 @@ import {ValueOf} from 'type-fest';
 
 type Icons = ValueOf<typeof MaterialIcons>;
 
-export interface DrawerEntryAttributes extends Attributes {
-  route: string;
+export interface DrawerEntryAttributes extends Attributes, ListItemLink {
   icon: Icons;
 }
 
 export class DrawerEntry<A extends DrawerEntryAttributes = DrawerEntryAttributes> extends Component<A> {
   view(vnode: Vnode<A>) {
     return (
-      <md-list-item-link headline={vnode.children as string} active={this.isRouteActive(vnode.attrs.route)} href={route(vnode.attrs.route)} onclick={this.navigateToRoute.bind(this)}>
+      <md-list-item-link headline={vnode.children as string} active={this.isRouteActive(vnode.attrs.href)} href={vnode.attrs.href} onclick={this.navigateToRoute.bind(this)} {...this.attrs.all()}>
         <MdIcon icon={vnode.attrs.icon} slot="start"/>
       </md-list-item-link>
     );
   }
 
-  isRouteActive(routeName: string) {
-    return route().current(routeName);
+  isRouteActive(href: string) {
+    return route(route().current()!) === href;
   }
 
   navigateToRoute(event: Event) {

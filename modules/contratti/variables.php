144be6cbd752de2e488491372858da8cfb3dc4c5
Pek5892 <pek5892@gmail.com>
2023-06-07 15:23:19 +0200

    Fix minore

--- modules/contratti/variables.php
+++ modules/contratti/variables.php
@@ -18,23 +18,25 @@
  */
 
 $r = $dbo->fetchOne('SELECT *,
+    co_contratti.nome AS nome_contratto,
     an_anagrafiche.pec,
     IF((an_referenti.email IS NOT NULL AND an_referenti.email != ""), an_referenti.email, an_anagrafiche.email) AS email,
     an_anagrafiche.idanagrafica,
-    an_referenti.nome,
+    an_referenti.nome AS nome_referente,
     an_anagrafiche.ragione_sociale
 FROM co_contratti
     INNER JOIN an_anagrafiche ON co_contratti.idanagrafica=an_anagrafiche.idanagrafica
-    LEFT OUTER JOIN an_referenti ON an_referenti.id=co_contratti.idreferente
+    LEFT JOIN an_referenti ON an_referenti.id=co_contratti.idreferente
 WHERE co_contratti.id='.prepare($id_record));
 
 // Variabili da sostituire
 return [
+    'nome' => $r['nome_contratto'],
     'email' => $options['is_pec'] ? $r['pec'] : $r['email'],
     'ragione_sociale' => $r['ragione_sociale'],
     'numero' => $r['numero'],
     'descrizione' => $r['descrizione'],
     'data' => Translator::dateToLocale($r['data_bozza']),
     'id_anagrafica' => $r['idanagrafica'],
-    'nome_referente' => $r['nome'],
+    'nome_referente' => $r['nome_referente'],
 ];

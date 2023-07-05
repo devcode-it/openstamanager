df52e1749aaa6eeebfef730f71fa1dee743b77e7
Pek5892 <pek5892@gmail.com>
2023-07-05 11:50:27 +0200

    Migliorie minori fatture di vendita

---
+++ update/2_4_48.sql
@@ -0,0 +1,1 @@
+UPDATE `zz_settings` SET `help` = "Modifica automaticamente la data di emissione di una fattura al momento della sua emissione, impostando data successiva a quella dell'ultima fattura emessa" WHERE `zz_settings`.`nome` = 'Data emissione fattura automatica';

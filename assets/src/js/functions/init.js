87be1cd6e1dee124641b565634b91be04af93d76
Pek5892 <pek5892@gmail.com>
2023-07-19 11:09:00 +0200

    Fix #1206

--- assets/src/js/functions/init.js
+++ assets/src/js/functions/init.js
@@ -43,6 +43,23 @@ function init() {
 
     if ($('form').length) {
         $('form').not('.no-check').parsley();
+        
+        CKEDITOR.on('instanceReady', function () {
+            $('form textarea').each(function () {
+              if ($(this).attr('obbligatorio') === "1") {
+                $(this).attr('required', '');
+              }
+            });
+          
+            $.each(CKEDITOR.instances, function (instance) {
+              CKEDITOR.instances[instance].on("change", function (e) {
+                for (instance in CKEDITOR.instances) {
+                  CKEDITOR.instances[instance].updateElement();
+                  $('form').parsley().validate();
+                }
+              });
+            });
+          });
     }
 
     // Aggiunta nell'URL del nome tab su cui tornare dopo il submit
--- modules/interventi/add.php
+++ modules/interventi/add.php
@@ -226,7 +226,8 @@ echo '
                 'label' => tr('Richiesta'),
                 'name' => 'richiesta',
                 'id' => 'richiesta_add',
-                'required' => '1',
+                'obbligatorio' => 1,
+                'required' => 1,
                 'value' => htmlentities($richiesta),
                 'extra' => 'style=\'max-height:80px;\'',
             ]);

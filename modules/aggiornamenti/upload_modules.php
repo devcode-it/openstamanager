783fd1a9c641977b54b6d4f96036337a4be6250c
Pek5892 <pek5892@gmail.com>
2024-04-05 14:32:29 +0200

    Fix upload moduli

--- modules/aggiornamenti/upload_modules.php
+++ modules/aggiornamenti/upload_modules.php
@@ -54,7 +54,7 @@ if (file_exists($extraction_dir.'/VERSION')) {
         ->ignoreVCS(true)
         ->in($extraction_dir);
 
-    $files_module = $finder->getTranslation('name')('MODULE');
+    $files_module = $finder->name('MODULE');
 
     foreach ($files_module as $file) {
         // Informazioni dal file di configurazione
@@ -69,8 +69,6 @@ if (file_exists($extraction_dir.'/VERSION')) {
             $table = 'zz_modules';
 
             $installed = Module::find((new Module())->getByField('name', $info['name']));
-            $insert['parent'] = (new Module())->getByField('name', $info['parent']);
-            $insert['icon'] = $info['icon'];
         }
 
         // Copia dei file nella cartella relativa
@@ -86,6 +84,8 @@ if (file_exists($extraction_dir.'/VERSION')) {
                 'order' => 100,
                 'default' => 0,
                 'enabled' => 1,
+                'icon' => $info['icon'],
+                'parent' => (new Module())->getByField('name', $info['parent']),
             ]));
             $id_record = $dbo->lastInsertedID();
             $dbo->insert($table.'_lang', array_merge($insert, [
@@ -107,7 +107,7 @@ if (file_exists($extraction_dir.'/VERSION')) {
         ->ignoreVCS(true)
         ->in($extraction_dir);
 
-    $files_plugin_template = $finder->getTranslation('name')('PLUGIN')->getTranslation('name')('TEMPLATES');
+    $files_plugin_template = $finder->name('PLUGIN')->name('TEMPLATES');
 
     foreach ($files_plugin_template as $file) {
         // Informazioni dal file di configurazione

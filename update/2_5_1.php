d23788ca8b47ee3df6b22f812961914e830d3fc5
Luca <lucas@devcode.it>
2024-04-12 18:03:03 +0200

    Se non è installato il modulo distinta base elimino il plugin

--- update/2_5_1.php
+++ update/2_5_1.php
@@ -10,3 +10,10 @@ if (empty($default)) {
     $adapter->is_default = 1;
     $adapter->save();
 }
+
+// Se non è installato il modulo distinta base elimino il plugin
+$fk = $database->fetchArray('SELECT * FROM `zz_plugins` WHERE `directory` = "distinta_base"');
+if (empty($fk)) {
+    // File e cartelle deprecate
+    delete(realpath(base_dir() . '/plugins/distinta_base/'));
+}

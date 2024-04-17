f9c5b4aa35afe76646ed595a1ed2f7d7c82f6aaa
Pek5892 <pek5892@gmail.com>
2024-04-17 11:21:05 +0200

    Aggiunti i campi marca e modello in Impianti

--- update/2_5_1.sql
+++ update/2_5_1.sql
@@ -233,3 +233,25 @@ INSERT INTO `zz_views` (`id_module`, `query`, `order`) VALUES
 ((SELECT `id_record` FROM `zz_modules_lang` WHERE `name` = 'Categorie documenti' LIMIT 1), '(SELECT GROUP_CONCAT(\' \', `nome`) FROM `zz_groups` WHERE `id` IN (SELECT `id_gruppo` FROM `do_permessi` WHERE `id_categoria` = `do_categorie`.`id`))', 5);
 INSERT INTO `zz_views_lang` (`id_lang`, `id_record`, `name`) VALUES
 (1, (SELECT MAX(`id`) FROM `zz_views` ), 'Gruppi abilitati');
+
+-- Aggiunta tabella my_impianti_marche
+CREATE TABLE IF NOT EXISTS `my_impianti_marche` (
+    `id` INT NOT NULL AUTO_INCREMENT , 
+    `name` TEXT NOT NULL,
+    PRIMARY KEY (`id`)
+); 
+
+ALTER TABLE `my_impianti` ADD `id_marca` INT NULL AFTER `idanagrafica`; 
+ALTER TABLE `my_impianti` ADD `modello` TEXT NULL AFTER `id_marca`; 
+
+-- Modulo Marche impianto
+INSERT INTO `zz_modules` (`directory`, `options`, `options2`, `icon`, `version`, `compatibility`, `order`, `parent`, `default`, `enabled`, `use_notes`, `use_checklists`) VALUES ('impianti_marche', 'SELECT |select| FROM `my_impianti_marche` WHERE 1=1 HAVING 2=2', '', 'fa fa-angle-right', '2.5.1', '2.5.1', '2', '30', '1', '1', '0', '0');
+INSERT INTO `zz_modules_lang` (`id_lang`, `id_record`, `name`, `title`) VALUES ('1', (SELECT MAX(id) FROM `zz_modules`), 'Marche impianti', 'Marche impianti');
+
+INSERT INTO `zz_views` (`id_module`, `query`, `order`) VALUES
+((SELECT `id_record` FROM `zz_modules_lang` WHERE `name` = 'Marche impianti' LIMIT 1), 'id', 1),
+((SELECT `id_record` FROM `zz_modules_lang` WHERE `name` = 'Marche impianti' LIMIT 1), 'name', 1);
+INSERT INTO `zz_views_lang` (`id_lang`, `id_record`, `name`) VALUES
+(1, (SELECT MAX(`id`)-1 FROM `zz_views` ), 'id'),
+(1, (SELECT MAX(`id`) FROM `zz_views` ), 'Nome');
+

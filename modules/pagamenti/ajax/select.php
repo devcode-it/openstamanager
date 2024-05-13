635ad11534ca3245c137edf03770680869f07955
Pek5892 <pek5892@gmail.com>
2024-05-13 10:42:59 +0200

    Fix ricerca metodo pagamento

--- modules/pagamenti/ajax/select.php
+++ modules/pagamenti/ajax/select.php
@@ -55,7 +55,7 @@ switch ($resource) {
         }
 
         if (!empty($search)) {
-            $search_fields[] = '`descrizione` LIKE '.prepare('%'.$search.'%');
+            $search_fields[] = '`title` LIKE '.prepare('%'.$search.'%');
         }
 
         $data = AJAX::selectResults($query, $where,

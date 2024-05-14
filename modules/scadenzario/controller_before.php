1d2d06b60d13e0abbcab8ec87d5232a17ad9c190
Pek5892 <pek5892@gmail.com>
2024-05-14 11:56:16 +0200

    Aggiunta legenda in Scadenzario

---
+++ modules/scadenzario/controller_before.php
@@ -0,0 +1,31 @@
+<?php
+
+echo '
+    <div class="pull-left">
+        <h5>'.tr('Legenda').':</h5>
+
+        <div class="legend">
+            <div class="icon" style="background-color:#CCFFCC;"></div>
+            <div class="text">Scadenza pagata</div>
+        </div>
+        <div class="legend">
+            <div class="icon" style="background-color:#ec5353;"></div>
+            <div class="text">Data concordata superata</div>
+        </div>
+        <div class="legend">
+            <div class="icon" style="background-color:#b3d2e3;"></div>
+            <div class="text">Data concordata</div>
+        </div>
+        <div class="legend">
+            <div class="icon" style="background-color:#f08080;"></div>
+            <div class="text">Scaduta</div>
+        </div>
+        <div class="legend">
+            <div class="icon" style="background-color:#f9f9c6;"></div>
+            <div class="text">Scadenza entro 10 giorni</div>
+        </div>
+        <div class="legend">
+            <div class="icon" style="background-color:#ffffff;"></div>
+            <div class="text">Scadenza futura</div>
+        </div>
+    </div>';

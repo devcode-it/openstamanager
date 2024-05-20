cb2da9f991bf735753758359f99d462bd2f1dbe3
Pek5892 <pek5892@gmail.com>
2024-05-20 11:12:08 +0200

    Fix salvataggio Stampe

--- modules/stampe/actions.php
+++ modules/stampe/actions.php
@@ -26,7 +26,7 @@ switch (post('op')) {
         }
 
         $print->setTranslation('title', post('title'));
-        $print->filename = post('filename');
+        $print->setTranslation('filename', post('filename'));
         $print->options = post('options');
         $print->order = post('order');
         $print->predefined = intval(post('predefined'));
@@ -34,7 +34,7 @@ switch (post('op')) {
 
         // Gestione file allegati
         $dbo->delete('zz_files_print', ['id_print' => $id_record]);
-        $id_files = (array) post('id_files');
+        $id_files = !empty(post('id_files')) ? post('id_files') : [];
         foreach ($id_files as $id_file) {
             $dbo->insert('zz_files_print', [
                 'id_print' => $id_record,

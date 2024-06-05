aac06d1c0fc9e176254a064c9ee83706e839335f
pek5892 <pek5892@gmail.com>
2024-06-05 12:48:27 +0200

    Fix tabella zz_groups

---
+++ update/2_5_3.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once __DIR__.'/core.php';
+
+// Controllo se è presente il campo nome in zz_groups
+$has_nome = database()->columnExists('zz_groups', 'nome');
+$has_name = database()->columnExists('zz_groups', 'name');
+
+if ($has_name && $has_nome) {
+    $database->query('ALTER TABLE `zz_groups` DROP `name`');
+} 

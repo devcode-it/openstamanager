60c308d72854ac1a42ed4f61ff24b95aa3207c5c
valentina <pek5892@gmail.com>
2024-06-24 12:17:32 +0200

    Fix minore

--- modules/fatture/src/Fattura.php
+++ modules/fatture/src/Fattura.php
@@ -535,7 +535,7 @@ class Fattura extends Document
     {
         $file = $this->getFatturaElettronica();
 
-        return !empty($this->progressivo_invio) && file_exists('files/fatture/'.$file->filename);
+        return !empty($this->progressivo_invio) && file_exists('files/'.$file->directory);
     }
 
     /**

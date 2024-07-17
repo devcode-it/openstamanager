d0ed5a28442fe8a416236bd3ed3035518f7f1143
valentina <pek5892@gmail.com>
2024-07-17 15:58:50 +0200

    Fix minore

--- plugins/contratti_anagrafiche/init.php
+++ plugins/contratti_anagrafiche/init.php
@@ -20,5 +20,5 @@
 include_once __DIR__.'/../../core.php';
 
 if (!empty($id_record)) {
-    $record = $dbo->fetchOne('SELECT *, `co_staticontratti`.`is_pianificabile` AS is_pianificabile FROM `co_contratti` INNER JOIN `co_stationtratti` ON `co_contratti`.`idstato` = `co_staticontratti`.`id` WHERE `co_contratti`.`id`='.prepare($id_record));
+    $record = $dbo->fetchOne('SELECT *, `co_staticontratti`.`is_pianificabile` AS is_pianificabile FROM `co_contratti` INNER JOIN `co_staticontratti` ON `co_contratti`.`idstato` = `co_staticontratti`.`id` WHERE `co_contratti`.`id`='.prepare($id_record));
 }

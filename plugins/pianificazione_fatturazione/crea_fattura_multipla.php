fe50530d4c505852fefbe20a09c8f2d3dbac3e5b
valentina <pek5892@gmail.com>
2024-12-02 10:22:55 +0100

    fix: select tipo di documento in fatturazione rate contrattuali

--- plugins/pianificazione_fatturazione/crea_fattura_multipla.php
+++ plugins/pianificazione_fatturazione/crea_fattura_multipla.php
@@ -69,7 +69,7 @@ echo '
 // Tipo di documento
 echo '
         <div class="col-md-6">
-            {[ "type": "select", "label": "'.tr('Tipo di fattura').'", "name": "idtipodocumento", "required": 1, "values": "query=SELECT * FROM `co_tipidocumento` LEFT JOIN `co_tipidocumento_lang` ON(`co_tipidocumento_lang`.`id_record` = `co_tipidocumento`.`id` AND `co_tipidocumento_lang`.`id_lang` = '.prepare(Models\Locale::getDefault()->id).') WHERE `dir`=\'entrata\'" ]}
+            {[ "type": "select", "label": "'.tr('Tipo di fattura').'", "name": "idtipodocumento", "required": 1, "values": "query=SELECT `co_tipidocumento`.id, `co_tipidocumento_lang`.`title` as descrizione FROM `co_tipidocumento` LEFT JOIN `co_tipidocumento_lang` ON(`co_tipidocumento_lang`.`id_record` = `co_tipidocumento`.`id` AND `co_tipidocumento_lang`.`id_lang` = '.prepare(Models\Locale::getDefault()->id).') WHERE `dir`=\'entrata\'" ]}
         </div>';
 
 // Sezionale

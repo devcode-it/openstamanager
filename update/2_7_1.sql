99f5bf8c07b1190bb11e0f9cbf673324f0bfd285
valentina <pek5892@gmail.com>
2025-02-24 15:00:53 +0100

    fix: allineamento pagamento e banche in rate fatture

--- update/2_7_1.sql
+++ update/2_7_1.sql
@@ -1,3 +1,5 @@
 -- Aggiornamento nome colonna Fornitore in Fornitore predefinito
 UPDATE `zz_views` SET `name` = 'Fornitore predefinito' WHERE `zz_views`.`name` = 'Fornitore' AND  `zz_views`.`id_module` = (SELECT `id` FROM `zz_modules` WHERE `name` = 'Articoli'); 
 UPDATE `zz_views_lang` SET `zz_views_lang`.`title` = 'Fornitore predefinito' WHERE `zz_views_lang`.`id_record` = (SELECT `id` FROM `zz_views` WHERE `name` = 'Fornitore predefinito' AND `zz_views`.`id_module` = (SELECT `id` FROM `zz_modules` WHERE `name` = 'Articoli')) AND `zz_views_lang`.`id_lang` = 1; 
+
+UPDATE `co_scadenziario` INNER JOIN `co_documenti` ON `co_scadenziario`.`iddocumento` = `co_documenti`.`id` SET `co_scadenziario`.`id_pagamento` = `co_documenti`.`idpagamento`, `co_scadenziario`.`id_banca_azienda` = `co_documenti`.`id_banca_azienda`, `co_scadenziario`.`id_banca_controparte` = `co_documenti`.`id_banca_controparte` WHERE `co_scadenziario`.`id_pagamento` = 0;

bcad094c1a9fb4847023b71d3805557d40ca5209
valentina <pek5892@gmail.com>
2025-03-04 12:50:25 +0100

    fix: correzione aliquote iva con codici null

---
+++ update/2_7_1.php
@@ -0,0 +1,15 @@
+<?php
+
+include __DIR__.'/../config.inc.php';
+
+$codici = database()->FetchArray('SELECT `id` FROM `co_iva` WHERE `deleted_at` IS NULL AND `codice` IS NULL');
+
+if (!empty($codici)) {
+    $max = database()->fetchOne('SELECT MAX(CAST(codice AS UNSIGNED)) AS max FROM co_iva WHERE deleted_at IS NULL')['max'];
+    $maxCodice = $max + 1;
+
+    foreach ($codici as $codice) {
+        database()->query('UPDATE co_iva SET codice = ' . $maxCodice . ' WHERE id = ' . $codice['id']);
+        $maxCodice++;
+    }
+}

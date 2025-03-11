2f2c743d6ccf16bf1b91cbb92b82816eedba747c
valentina <pek5892@gmail.com>
2025-03-11 17:07:44 +0100

    fix: spostamento files fatture in cartella /files/vendite

---
+++ update/2_7_2.php
@@ -0,0 +1,20 @@
+<?php
+
+include __DIR__.'/../config.inc.php';
+$module = \Models\Module::where('name', 'Fatture di vendita')->first();
+
+$directory = 'files/fatture/';
+$files = glob($directory.'*.xml');
+
+$new_folder = 'files/'.$module->attachments_directory.'/';
+
+$attachments = database()->fetchArray('SELECT `filename` FROM `zz_files` WHERE `name` = "Fattura Elettronica" AND `id_module` = '.$module->id);
+
+$attachments_filenames = array_column($attachments, 'filename');
+
+foreach ($files as $file) {
+    $filename = basename($file);
+    if (in_array($filename, $attachments_filenames)) {
+        rename($file, $new_folder.$filename);
+    }
+}

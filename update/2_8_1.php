47e44196ecf760ddd00cf01fdef2954766feeeac
valentina <pek5892@gmail.com>
2025-06-10 10:39:09 +0200

    fix: impostazione categoria files

--- update/2_8_1.php
+++ update/2_8_1.php
@@ -2,7 +2,7 @@
 
 include __DIR__.'/../config.inc.php';
 
-$id_categoria = $dbo->query('SELECT `id` FROM `zz_files_categories` WHERE `name` = \'fattura elettronica\'');
+$id_categoria = $dbo->fetchOne('SELECT `id` FROM `zz_files_categories` WHERE `name` = \'fattura elettronica\'');
 if (empty($id_categoria)) {
-    $dbo->query('INSERT INTO `zz_files_categories` (`name`) VALUES (\'fattura elettronica\')');
+    $dbo->query('INSERT INTO `zz_files_categories` (`name`) VALUES (\'Fattura elettronica\')');
 }

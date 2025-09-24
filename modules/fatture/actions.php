3790b77216da642fa01958dbc87a8812dea38be9
FabioL <1647128+loviuz@users.noreply.github.com>
2025-09-24 15:33:51 +0200

    fix: salvataggio barcode riga solo se barcode

--- modules/fatture/actions.php
+++ modules/fatture/actions.php
@@ -1052,11 +1052,13 @@ switch ($op) {
     case 'add_articolo':
         $id_articolo = post('id_articolo');
         $barcode = post('barcode');
+        $save_inline_barcode = true;
 
         if (!empty($barcode)) {
             $id_articolo = $dbo->selectOne('mg_articoli_barcode', 'idarticolo', ['barcode' => $barcode])['idarticolo'];
             if (empty($id_articolo)) {
                 $id_articolo = $dbo->selectOne('mg_articoli', 'id', ['deleted_at' => null, 'attivo' => 1, 'barcode' => '', 'codice' => $barcode])['id'];
+                $save_inline_barcode = false;
             }
         }
 
@@ -1074,7 +1076,11 @@ switch ($op) {
                 $qta = 1;
 
                 $articolo->um = $originale->um;
-                $articolo->barcode = $barcode;
+
+                if ($save_inline_barcode) {
+                    $articolo->barcode = $barcode;
+                }
+
                 $articolo->qta = 1;
                 $articolo->costo_unitario = $originale->prezzo_acquisto;
 

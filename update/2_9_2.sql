b9015bb7933c8120362a5e643fffb458d33e9e0d
valentina <pek5892@gmail.com>
2025-09-25 15:59:21 +0200

    fix: foreign key per installazione

--- update/2_9_2.sql
+++ update/2_9_2.sql
@@ -115,8 +115,6 @@ ALTER TABLE `co_righe_documenti` ADD CONSTRAINT `co_righe_documenti_ibfk_5` FORE
 ALTER TABLE `co_righe_documenti` ADD CONSTRAINT `co_righe_documenti_ibfk_6` FOREIGN KEY (`idintervento`) REFERENCES `in_interventi`(`id`) ON DELETE SET NULL;
 
 -- FOREIGN KEYS per tabelle movimenti
-DELETE FROM `co_movimenti` WHERE `iddocumento` NOT IN (SELECT `id` FROM `co_documenti`);
-ALTER TABLE `co_movimenti` ADD CONSTRAINT `co_movimenti_ibfk_2` FOREIGN KEY (`iddocumento`) REFERENCES `co_documenti`(`id`) ON DELETE CASCADE;
 ALTER TABLE `co_movimenti` ADD CONSTRAINT `co_movimenti_ibfk_3` FOREIGN KEY (`id_anagrafica`) REFERENCES `an_anagrafiche`(`idanagrafica`) ON DELETE SET NULL;
 
 -- FOREIGN KEYS per tabelle scadenziario

1eb8da9b67ec0562984ba678abe8bec3b77ea490
valentina <pek5892@gmail.com>
2025-10-03 15:32:05 +0200

    fix: salvataggio conti in plugin Registrazioni

--- plugins/registrazioni/actions.php
+++ plugins/registrazioni/actions.php
@@ -34,24 +34,71 @@ switch ($operazione) {
         $conti_cespiti = (array) post('idconto_cespiti');
         $cespiti = (array) post('is_cespite');
 
-        foreach ($conti as $id_riga => $conto) {
-            $riga = RigaArticolo::find($id_riga) ?: Riga::find($id_riga);
-            $riga = $riga ?: Sconto::find($id_riga);
+        $errori = [];
+        $righe_aggiornate = 0;
 
-            // Modifica descrizione fornitore
-            $riga->id_conto = $cespiti[$id_riga] ? $conti_cespiti[$id_riga] : $conto;
-            $riga->is_cespite = $cespiti[$id_riga] ?? 0;
-            $riga->save();
+        try {
+            foreach ($conti as $id_riga => $conto) {
+                $riga = RigaArticolo::find($id_riga) ?: Riga::find($id_riga);
+                $riga = $riga ?: Sconto::find($id_riga);
+
+                if (!$riga) {
+                    $errori[] = tr('Riga _ID_ non trovata', ['_ID_' => $id_riga]);
+                    continue;
+                }
+
+                $is_cespite = !empty($cespiti[$id_riga]);
+                $conto_selezionato = $is_cespite ? ($conti_cespiti[$id_riga] ?? null) : $conto;
+
+                // Validazione lato server
+                if (empty($conto_selezionato)) {
+                    $tipo_conto = $is_cespite ? tr('conto cespite') : tr('conto');
+                    $errori[] = tr('Riga _ID_: selezionare un _TIPO_', [
+                        '_ID_' => $id_riga,
+                        '_TIPO_' => $tipo_conto
+                    ]);
+                    continue;
+                }
+
+                // Verifica che il conto esista nel database
+                $conto_exists = $dbo->fetchOne('SELECT id FROM co_pianodeiconti3 WHERE id = ?', [$conto_selezionato]);
+                if (!$conto_exists) {
+                    $errori[] = tr('Riga _ID_: il conto selezionato non è valido', ['_ID_' => $id_riga]);
+                    continue;
+                }
+
+                // Aggiorna la riga
+                $riga->id_conto = $conto_selezionato;
+                $riga->is_cespite = $is_cespite ? 1 : 0;
+                $riga->save();
+
+                $righe_aggiornate++;
+            }
+
+            if (!empty($errori)) {
+                flash()->error(tr('Errori durante il salvataggio:').'<ul><li>'.implode('</li><li>', $errori).'</li></ul>');
+            } else {
+                $fattura = Fattura::find($id_record);
+
+                if ($fattura->stato->getTranslation('title') != 'Bozza') {
+                    try {
+                        $fattura->gestoreMovimenti = new GestoreMovimenti($fattura);
+                        $fattura->gestoreMovimenti->registra();
+                    } catch (Exception $e) {
+                        flash()->error(tr('Errore durante la registrazione dei movimenti contabili: _ERROR_', ['_ERROR_' => $e->getMessage()]));
+                        break;
+                    }
+                }
+
+                if ($righe_aggiornate > 0) {
+                    flash()->info(tr('Salvataggio completato! _NUM_ righe aggiornate.', ['_NUM_' => $righe_aggiornate]));
+                } else {
+                    flash()->warning(tr('Nessuna riga è stata aggiornata.'));
+                }
+            }
+        } catch (Exception $e) {
+            flash()->error(tr('Errore durante il salvataggio: _ERROR_', ['_ERROR_' => $e->getMessage()]));
         }
-
-        $fattura = Fattura::find($id_record);
-
-        if ($fattura->stato->getTranslation('title') != 'Bozza') {
-            $fattura->gestoreMovimenti = new GestoreMovimenti($fattura);
-            $fattura->gestoreMovimenti->registra();
-        }
-
-        flash()->info(tr('Salvataggio completato!'));
 
         break;
 }

09bcc322aba8e321465952c53348ec74e866d5d1
valentina <pek5892@gmail.com>
2025-10-06 11:27:46 +0200

    fix: query vista giacenze

--- update/2_9_3.sql
+++ update/2_9_3.sql
@@ -154,7 +154,7 @@ UPDATE `zz_settings` INNER JOIN `zz_settings_lang` ON `zz_settings`.`id` = `zz_s
 UPDATE `zz_settings` INNER JOIN `zz_settings_lang` ON `zz_settings`.`id` = `zz_settings_lang`.`id_record` SET `zz_settings_lang`.`help` = "Variables availables: {email} {numero} {ragione_sociale} {richiesta} {descrizione} {data} {data richiesta} {data fine intervento} {id_anagrafica} {stato} {tipo}" WHERE `zz_settings`.`nome` = 'Descrizione personalizzata in fatturazione' AND `id_lang` = 2;
 
 
-UPDATE `zz_views` LEFT JOIN `zz_modules` ON `zz_views`.`id_module` = `zz_modules`.`id` SET `query` = "IF(giacenze.stato_giacenza>0, '#CCFFCC', '#ec5353')" WHERE `zz_views`.`name` = '_bg_' AND `zz_modules`.`name` = 'Articoli';
+UPDATE `zz_views` LEFT JOIN `zz_modules` ON `zz_views`.`id_module` = `zz_modules`.`id` SET `query` = "IF(giacenze.stato_giacenza = 0, '', IF(giacenze.stato_giacenza > 0, '#CCFFCC', '#ec5353'))" WHERE `zz_views`.`name` = '_bg_' AND `zz_modules`.`name` = 'Articoli';
 
 -- fix: invio via mail token otp
 ALTER TABLE `em_emails` CHANGE `created_by` `created_by` INT(11) NULL;

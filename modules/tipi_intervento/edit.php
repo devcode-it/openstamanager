f27e0b8079edd79dd8de569b0bab2bbc29837a77
valentina <pek5892@gmail.com>
2025-10-13 15:20:52 +0200

    fix: allineamento versione

--- modules/tipi_intervento/edit.php
+++ modules/tipi_intervento/edit.php
@@ -19,9 +19,6 @@
 
 include_once __DIR__.'/../../core.php';
 
-$id_tipi = $dbo->select('in_tipiintervento_tipologie', '*', [], ['idtipointervento' => $id_record]);
-$id_tipi = implode(',', array_column($id_tipi, 'tipo'));
-
 ?><form action="" method="post" id="edit-form">
 	<input type="hidden" name="op" value="update">
 	<input type="hidden" name="backto" value="record-edit">

8c14a9f96e2dd115594f98463bb1c8c2700e9db6
valentina <pek5892@gmail.com>
2025-11-17 14:34:41 +0100

    fix: segnalazione compatibilità

---
+++ update/2_9_6.sql
@@ -0,0 +1,6 @@
+UPDATE `zz_modules` SET `version` = '2.9.6' WHERE `name` = 'Automezzi';
+UPDATE `zz_modules` SET `compatibility` = '2.9.6' WHERE `name` = 'Automezzi';
+UPDATE `zz_plugins` SET `version` = '2.9.6' WHERE `name` = 'Importazione FE';
+UPDATE `zz_plugins` SET `compatibility` = '2.9.6' WHERE `name` = 'Importazione FE';
+UPDATE `zz_modules` SET `default` = 1 WHERE `name` = 'Automezzi';
+UPDATE `zz_plugins` SET `default` = 1 WHERE `name` = 'Importazione FE';

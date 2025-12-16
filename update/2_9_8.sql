e37a61b145958aac30ddb45312f765d8ea4b0316
valentina <pek5892@gmail.com>
2025-12-16 16:42:57 +0100

    fix: allineamento vista Accesso con OAuth

--- update/2_9_8.sql
+++ update/2_9_8.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `zz_oauth2` CHANGE `nome` `name` VARCHAR(255) NOT NULL; 
+
+UPDATE `zz_views` SET `query` = 'name' WHERE `name` = 'Nome' AND `id_module` = (SELECT `id` FROM `zz_modules` WHERE `name` = 'Accesso con OAuth');

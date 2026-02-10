379a247227f49de2b035965ae69b24c79eb049f1
valentina <pek5892@gmail.com>
2026-02-10 10:38:50 +0100

    fix: redirect a indirizzo

--- lib/functions.php
+++ lib/functions.php
@@ -44,7 +44,8 @@ function redirect_url($url, $type = 'php')
     switch ($type) {
         case 'php':
             header('Location: '.$url);
-            break;
+            exit();
+
         case 'js':
             echo '<script type="text/javascript">location.href="'.$url.'";</script>';
             break;

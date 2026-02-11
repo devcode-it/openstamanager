c6d35757b00c08263410052c91a878d11cc14c19
valentina <pek5892@gmail.com>
2026-02-11 13:08:49 +0100

    fix: generazione fattura elettronica per sedi committente paesi esteri

--- plugins/exportFE/src/FatturaElettronica.php
+++ plugins/exportFE/src/FatturaElettronica.php
@@ -1125,29 +1125,32 @@ class FatturaElettronica implements \Stringable
         if (!empty($documento['idsede_destinazione'])) {
             $sede = $database->fetchOne('SELECT * FROM an_sedi WHERE id='.prepare($documento['idsede_destinazione']));
 
-            $is_nazione_it = false;
-            if (!empty($sede['id_nazione'])) {
-                $rs_nazione = Nazione::find($sede['id_nazione']);
-                $is_nazione_it = ($rs_nazione['iso2'] == "IT") ? true : false;
-            }
-
             if (!empty($sede['indirizzo'])) {
                 $result['IndirizzoResa']['Indirizzo'] = $sede['indirizzo'];
             }
 
-            if (!empty($sede['cap'])) {
-                $result['IndirizzoResa']['CAP'] = ($is_nazione_it == true) ? $sede['cap'] : "00000";
+            // CAP: per nazioni estere usare '00000', altrimenti il CAP reale
+            if (!empty($sede['id_nazione'])) {
+                $rs_nazione = Nazione::find($sede['id_nazione']);
+                $result['IndirizzoResa']['CAP'] = ($rs_nazione['iso2'] == 'IT') ? $sede['cap'] : '00000';
+            } elseif (!empty($sede['cap'])) {
+                $result['IndirizzoResa']['CAP'] = $sede['cap'];
             }
 
             if (!empty($sede['citta'])) {
                 $result['IndirizzoResa']['Comune'] = $sede['citta'];
             }
 
-            if (!empty($sede['provincia']) && $is_nazione_it == true) {
-                $result['IndirizzoResa']['Provincia'] = $sede['provincia'];
+            // Provincia impostata SOLO SE nazione ITALIA
+            if (!empty($sede['provincia']) && !empty($sede['id_nazione'])) {
+                $rs_nazione = Nazione::find($sede['id_nazione']);
+                if ($rs_nazione['iso2'] == 'IT') {
+                    $result['IndirizzoResa']['Provincia'] = strtoupper((string) $sede['provincia']);
+                }
             }
 
             if (!empty($sede['id_nazione'])) {
+                $rs_nazione = Nazione::find($sede['id_nazione']);
                 $result['IndirizzoResa']['Nazione'] = $rs_nazione['iso2'];
             }
         }

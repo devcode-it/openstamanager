5561216009b6e1c084679ac82cd49e1f0fc880ea
valentina <pek5892@gmail.com>
2026-03-16 12:04:03 +0100

    fix: query vista modulo Stati dei preventivi

--- update/2_10_2.sql
+++ update/2_10_2.sql
@@ -1,2 +1,14 @@
 -- Aggiornamento campo uid di interventi tecnici
 ALTER TABLE `in_interventi_tecnici` CHANGE `uid` `uid` VARCHAR(255) NULL DEFAULT NULL;
+
+-- Allineamento vista Stati dei preventivi
+UPDATE `zz_modules` SET `options` = "
+SELECT
+    |select| 
+FROM 
+    `co_statipreventivi`
+    LEFT JOIN `co_statipreventivi_lang` ON (`co_statipreventivi`.`id` = `co_statipreventivi_lang`.`id_record` AND |lang|)
+WHERE 
+    1=1 AND deleted_at IS NULL
+HAVING 
+    2=2" WHERE `name` = 'Stati dei preventivi';

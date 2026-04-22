614336fcdd1c4946ff2d346e86d56fc1eb46958a
valentina <pek5892@gmail.com>
2026-04-22 15:08:57 +0200

    fix: addebito spese di incasso

--- modules/fatture/src/Fattura.php
+++ modules/fatture/src/Fattura.php
@@ -854,7 +854,7 @@ class Fattura extends Document
 
     public function manageRigaSpeseIncasso()
     {
-        if ($this->tipo->dir == 'uscita' || $this->stato->descrizione != 'Bozza') {
+        if ($this->tipo->dir == 'uscita' || $this->stato->name != 'Bozza') {
             return null;
         }
 

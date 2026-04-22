0e8f3c08a1b5d7f3422f6b49f22ff47d17f92ff6
valentina <pek5892@gmail.com>
2026-04-22 09:00:54 +0200

    fix: avviso fatturazione attività futura per date corrispondenti

--- modules/fatture/edit.php
+++ modules/fatture/edit.php
@@ -235,12 +235,11 @@ if ($dir == 'entrata') {
 
     // Verifica la data dell'intervento rispetto alla data della fattura
     $fatturazione_futura = false;
-    $data_fattura = new Carbon($fattura->data);
+    $data_fattura = (new Carbon($fattura->data))->startOfDay();
     $interventi_collegati = $fattura->getDocumentiCollegati()[Intervento::class];
     if (!empty($interventi_collegati)) {
         foreach ($interventi_collegati as $intervento) {
-            $fine_intervento = $intervento->fine;
-            $fine_intervento = new Carbon($fine_intervento);
+            $fine_intervento = (new Carbon($intervento->fine))->startOfDay();
 
             if ($fine_intervento->diffInDays($data_fattura, false) < 0) {
                 $fatturazione_futura = true;

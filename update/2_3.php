e7a3f0f0d304a7f752dce4eea3bf66741031ad3b
valentina <pek5892@gmail.com>
2026-05-11 11:33:10 +0200

    fix: retrofix per compatibilità mysql8.4

--- update/2_3.php
+++ update/2_3.php
@@ -8,14 +8,8 @@
 $database->query("UPDATE mg_articoli SET contenuto = REPLACE(REPLACE(REPLACE(contenuto, '&quot;', '\"'), '\n', ".prepare(PHP_EOL)."), '`', '\"')");
 $database->query("UPDATE my_impianto_componenti SET contenuto = REPLACE(REPLACE(REPLACE(contenuto, '&quot;', '\"'), '\n', ".prepare(PHP_EOL)."), '`', '\"')");
 
-// Fix per la presenza della Foreign Key in in_interventi_tecnici
-$fk = $database->fetchArray('SELECT TABLE_NAME,COLUMN_NAME,CONSTRAINT_NAME, REFERENCED_TABLE_NAME,REFERENCED_COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE REFERENCED_TABLE_SCHEMA = '.prepare($database->getDatabaseName())." AND REFERENCED_TABLE_NAME = 'in_interventi' AND CONSTRAINT_NAME = 'in_interventi_tecnici_ibfk_1'");
-if (!empty($fk)) {
-    $database->query('ALTER TABLE `in_interventi_tecnici` DROP FOREIGN KEY `in_interventi_tecnici_ibfk_1`');
-}
-
-$database->query('ALTER TABLE `in_interventi` DROP PRIMARY KEY, CHANGE `idintervento` `codice` varchar(25) NOT NULL UNIQUE, ADD PRIMARY KEY (`id`)');
-$database->query('DROP INDEX primary_key ON `in_interventi`');
+// Il DROP PRIMARY KEY, CHANGE idintervento->codice e ADD PRIMARY KEY è stato spostato in update/2_3.sql
+// Anche la rimozione della FOREIGN KEY in_interventi_tecnici_ibfk_1 è stata spostata in update/2_3.sql
 $database->query('UPDATE `in_interventi_tecnici` SET `idintervento` = (SELECT `id` FROM `in_interventi` WHERE `in_interventi`.`codice` = `in_interventi_tecnici`.`idintervento`)');
 $database->query('ALTER TABLE `in_interventi_tecnici` CHANGE `idintervento` `idintervento` varchar(25)');
 $database->query("UPDATE `in_interventi_tecnici` SET `idintervento` = NULL WHERE `idintervento` = 0 OR `idintervento` = ''");

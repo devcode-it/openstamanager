65a8e5b4f685d5a08b3fa5f1f52ee4f02d5bc001
valentina <pek5892@gmail.com>
2026-06-12 08:55:01 +0200

    fix: compatibilità php <= 8.5

--- src/Models/OAuth2.php
+++ src/Models/OAuth2.php
@@ -32,10 +32,8 @@ class OAuth2 extends Model
 
     protected $provider;
 
-    #[\Override]
     protected $table = 'zz_oauth2';
 
-    #[\Override]
     protected $casts = [
         'config' => 'array',
     ];

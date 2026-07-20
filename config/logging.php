74dcd4ffc57bf80f5d6f2ab7af2337e55f1cd015
Dasc3er <Dasc3er@users.noreply.github.com>
2026-07-20 11:57:52 +0200

    Revert "Fix percorsi log Laravel"
    This reverts commit 6d02b4db64c8a145e27abd7ce9b9a669a15dd2af.

--- config/logging.php
+++ config/logging.php
@@ -68,14 +68,14 @@ return [
          */
         'single' => [
             'driver' => 'single',
-            'path' => rtrim(base_path_osm(), '/').'/logs/app.log',
+            'path' => base_path_osm().'logs/app.log',
             'level' => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
         ],
 
         'daily' => [
             'driver' => 'daily',
-            'path' => rtrim(base_path_osm(), '/').'/logs/app.log',
+            'path' => base_path_osm().'logs/app.log',
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => env('LOG_DAILY_DAYS', 30),
             'replace_placeholders' => true,

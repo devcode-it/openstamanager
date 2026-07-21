0298f84b00012b76d9e33cf6613e427bcf677d82
valentina <pek5892@gmail.com>
2026-07-21 09:18:54 +0200

    fix: query vista articoli per escludere cespiti da q.tà

--- update/2_12.sql
+++ update/2_12.sql
@@ -70,14 +70,15 @@ ALTER TABLE `an_anagrafiche` ADD INDEX `idx_id_conto_cliente` (`id_conto_cliente
 ALTER TABLE `an_anagrafiche` ADD INDEX `idx_id_conto_fornitore` (`id_conto_fornitore`);
 
 
+
 UPDATE `zz_views`
 LEFT JOIN `zz_modules` ON `zz_modules`.`id` = `zz_views`.`id_module`
-SET `zz_views`.`query` = '`mg_articoli`.`qta`-IFNULL((SELECT IFNULL(SUM(`mg_movimenti`.`qta`), 0) FROM `mg_movimenti` WHERE `mg_movimenti`.`id_articolo` = `mg_articoli`.`id` AND `mg_movimenti`.`id_sede` = (SELECT `valore` FROM `zz_settings` WHERE `nome` = ''Magazzino cespiti'')), 0)'
+SET `zz_views`.`query` = 'IFNULL((SELECT SUM(`mg_movimenti`.`qta`) FROM `mg_movimenti` WHERE `mg_movimenti`.`id_articolo` = `mg_articoli`.`id`), 0)-IFNULL((SELECT IFNULL(SUM(`mg_movimenti`.`qta`), 0) FROM `mg_movimenti` WHERE `mg_movimenti`.`id_articolo` = `mg_articoli`.`id` AND `mg_movimenti`.`id_sede` = (SELECT NULLIF(`valore`, '''') FROM `zz_settings` WHERE `nome` = ''Magazzino cespiti'')), 0)'
 WHERE `zz_views`.`name` = 'Q.tà' AND `zz_modules`.`name` = 'Articoli';
 
 UPDATE `zz_views`
 LEFT JOIN `zz_modules` ON `zz_modules`.`id` = `zz_views`.`id_module`
-SET `zz_views`.`query` = '`mg_articoli`.`qta`-IFNULL(a.qta_impegnata, 0)-IFNULL((SELECT IFNULL(SUM(`mg_movimenti`.`qta`), 0) FROM `mg_movimenti` WHERE `mg_movimenti`.`id_articolo` = `mg_articoli`.`id` AND `mg_movimenti`.`id_sede` = (SELECT `valore` FROM `zz_settings` WHERE `nome` = ''Magazzino cespiti'')), 0)'
+SET `zz_views`.`query` = 'IFNULL((SELECT SUM(`mg_movimenti`.`qta`) FROM `mg_movimenti` WHERE `mg_movimenti`.`id_articolo` = `mg_articoli`.`id`), 0)-IFNULL(a.qta_impegnata, 0)-IFNULL((SELECT IFNULL(SUM(`mg_movimenti`.`qta`), 0) FROM `mg_movimenti` WHERE `mg_movimenti`.`id_articolo` = `mg_articoli`.`id` AND `mg_movimenti`.`id_sede` = (SELECT NULLIF(`valore`, '''') FROM `zz_settings` WHERE `nome` = ''Magazzino cespiti'')), 0)'
 WHERE `zz_views`.`name` = 'Q.tà disponibile' AND `zz_modules`.`name` = 'Articoli';
 
 -- Rimossa condizione per poter visualizzare anche le Stampe non attive

89433a3a7d24455f2307f86e0626b114775bcf61
Sajo <75281007+sajotrei@users.noreply.github.com>
2026-08-10 00:03:41 +0200

    Aggiunge intestazione GPL e autore alla stampa Note spese

--- templates/note_spese/init.php
+++ templates/note_spese/init.php
@@ -1,5 +1,30 @@
 <?php
 
+/*
+ * OpenSTAManager: il software gestionale open source per l'assistenza tecnica e la fatturazione
+ * Copyright (C) DevCode s.r.l.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Stampa riepilogativa del modulo Note spese.
+ *
+ * @author sajotrei
+ * @link https://github.com/sajotrei
+ */
+
 include_once __DIR__.'/../../core.php';
 
 // La stampa e' di periodo e non e' legata a una singola anagrafica/documento.

a4915cb3bf187048d6a3ab18d9102b8b9bcaf33e
Sajo <75281007+sajotrei@users.noreply.github.com>
2026-08-17 13:17:31 +0200

    chore: usa la migrazione 2.12 esistente

--- update/2_12_1.sql
+++
@@ -1,4 +0,0 @@
--- Associazione automezzo alle singole sessioni delle attività (#1693)
-ALTER TABLE `in_interventi_tecnici`
-    ADD `id_automezzo` INT NULL AFTER `id_tecnico`,
-    ADD INDEX `idx_in_interventi_tecnici_automezzo` (`id_automezzo`);
